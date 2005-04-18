c93f1d4f15a9be4503b63db8e3036e1d7f19d154
Daniel Ostermeier <daniel@zutubi.com>
2005-04-18 13:30:20 +0000

    consolidating code

--- src/main/java/com/cinnamonbob/command/Bootstrap.java
+++ src/main/java/com/cinnamonbob/command/Bootstrap.java
@@ -1,72 +1,92 @@
 package com.cinnamonbob.command;
 
 import com.cinnamonbob.BobServerProxy;
-import com.townleyenterprises.command.CommandOption;
-import com.townleyenterprises.command.CommandParser;
-import com.townleyenterprises.command.DefaultCommandListener;
+import com.cinnamonbob.BobServer;
+import com.cinnamonbob.ShutdownService;
+import com.cinnamonbob.util.IOHelper;
+
+import java.net.Socket;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Vector;
+
+import org.apache.xmlrpc.XmlRpcClient;
 
 /**
  * @author Daniel Ostermeier
  */
 public class Bootstrap
 {
+
+    private static final Logger LOG = Logger.getLogger(Bootstrap.class.getName());
+
     //TODO: support command the form ... options COMMAND command-options ARGUMENTS
 
     // server connection details.
     private int port = 2345;
     private String host = "localhost";
 
-    private CommandOption start = new CommandOption("start",
-            's', false, null,
-            "start the cinnamonbob build server")
-    {
-        public void execute() throws Exception
-        {
-            BobServerProxy server = new BobServerProxy(host, port);
-            server.start();
-        }
-    };
-
-    private CommandOption stop = new CommandOption("stop",
-            'x',
-            false,
-            null,
-            "stop the cinnamonbob build server")
-    {
-        public void execute() throws Exception
-        {
-            BobServerProxy server = new BobServerProxy(host, port);
-            server.stop();
-        }
-    };
-
-    private CommandOption build = new CommandOption("build",
-            'b',
-            true,
-            "[ PROJECT-NAME ]",
-            "build the specified project")
-    {
-        public void execute() throws Exception
-        {
-            BobServerProxy server = new BobServerProxy(host, port);
-            server.build(getArg());
-        }
-    };
-
     public void parse(String args[]) throws Exception
     {
-        CommandParser parser = new CommandParser("bob");
-        parser.addCommandListener(new DefaultCommandListener("Start options", new CommandOption[]{start}));
-        parser.addCommandListener(new DefaultCommandListener("Stop options", new CommandOption[]{stop}));
-        parser.addCommandListener(new DefaultCommandListener("Build options", new CommandOption[]{build}));
+        if (args.length == 0)
+        {
+            return;
+        }
 
-        parser.parse(args);
-
-        parser.executeCommands();
+        //TODO: support configuration of host and port settings.
+        String command = args[0];
+        if ("start".equals(command))
+        {
+            start();
+        } else if ("stop".equals(command))
+        {
+            stop();
+        } else if ("build".equals(command))
+        {
+            build(args[1]);
+        }
     }
 
     public static void main(String argv[]) throws Exception
     {
         new Bootstrap().parse(argv);
     }
+
+    public void start() throws Exception
+    {
+        BobServer server = new BobServer(port);
+        server.start();
+    }
+
+    public void stop()
+    {
+        // connect to the admin port and send the shutdown command...
+        Socket socket = null;
+        try {
+            socket = new Socket(host, port);
+
+            // send the shutdown command.
+            OutputStream out = socket.getOutputStream();
+            out.write(ShutdownService.Command.SHUTDOWN.getBytes());
+            out.close();
+
+        } catch (IOException e) {
+            LOG.log(Level.WARNING, "Unable to send shutdown request", e);
+
+        } finally {
+            IOHelper.close(socket);
+        }
+
+    }
+
+    public void build(String projectName) throws Exception
+    {
+        XmlRpcClient client = new XmlRpcClient("http://localhost:8080/api/xmlrpc");
+        Vector<String> params = new Vector<String>();
+        params.add(projectName);
+        client.execute("build", params);
+    }
+
 }
