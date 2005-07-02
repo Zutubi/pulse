2198b8fd088814f76237bd6931aae7dba7fb36d8
Daniel Ostermeier <daniel@zutubi.com>
2005-07-02 06:56:10 +0000

    exception migrated from core

---
+++ src/main/java/com/cinnamonbob/core2/InternalBuildFailureException.java
@@ -0,0 +1,42 @@
+package com.cinnamonbob.core2;
+
+import com.cinnamonbob.BobRuntimeException;
+
+/**
+ * An InternalBuildFailureException is raised when something catastrophic
+ * happens during a project build.  It is "normal" for builds to fail, this
+ * is just recorded in the result.  This exception is used when terrible
+ * things like I/O errors occur when trying to, for example, store command
+ * output.
+ * 
+ * @author jsankey
+ */
+public class InternalBuildFailureException extends BobRuntimeException
+{
+    private static final long serialVersionUID = 3257005458144178485L;
+    
+    //=========================================================================
+    // Construction
+    //=========================================================================
+    
+    public InternalBuildFailureException(String message, Exception cause)
+    {
+        super(message, cause);
+    }
+
+    
+    public InternalBuildFailureException(String message)
+    {
+        super(message);
+    }
+    
+    public InternalBuildFailureException(Exception cause)
+    {
+        super(cause);
+    }
+    
+    public InternalBuildFailureException()
+    {
+        super();
+    }
+}
