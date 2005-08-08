b6482a4effba21b460af367bf02aca33e143ba94
Daniel Ostermeier <daniel@zutubi.com>
2005-08-08 10:24:18 +0000

    table names are uppercase.

--- src/main/java/com/cinnamonbob/bootstrap/DatabaseBootstrap.java
+++ src/main/java/com/cinnamonbob/bootstrap/DatabaseBootstrap.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  */
 public class DatabaseBootstrap
 {
-    public static final String SCHEMA_TEST_TABLE = "User";
+    public static final String SCHEMA_TEST_TABLE = "USER";
 
     public DatabaseBootstrap()
     {
