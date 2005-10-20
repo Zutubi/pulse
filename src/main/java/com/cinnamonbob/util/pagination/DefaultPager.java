69a84e69ee0e2fe9fa66d7c24687e07a7742ea14
Daniel Ostermeier <daniel@zutubi.com>
2005-10-20 12:01:11 +0000

    Fix compile error :|

--- src/main/java/com/cinnamonbob/util/pagination/DefaultPager.java
+++ src/main/java/com/cinnamonbob/util/pagination/DefaultPager.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * <class-comment/>
  */
-public class DefaultPager implements prototype.pagination.Pager
+public class DefaultPager implements Pager
 {
     private final List data;
 
