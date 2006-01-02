c1add7cc84b5d256a2958289e870929eb6c62465
Daniel Ostermeier <daniel@zutubi.com>
2006-01-02 00:22:08 +0000

    CIB-72: edit slave.

--- master/src/java/com/cinnamonbob/web/EditSlaveAction.java
+++ master/src/java/com/cinnamonbob/web/EditSlaveAction.java
@@ -1,8 +1,52 @@
 package com.cinnamonbob.web;
 
+import com.cinnamonbob.model.persistence.SlaveDao;
+import com.cinnamonbob.model.Slave;
+
 /**
  * <class-comment/>
  */
-public class EditSlaveAction
+public class EditSlaveAction extends ActionSupport
 {
+    private SlaveDao dao;
+
+    private long id;
+    private Slave slave = new Slave();
+
+    public void setSlaveDao(SlaveDao dao)
+    {
+        this.dao = dao;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public Slave getSlave()
+    {
+        return slave;
+    }
+
+    public String doDefault()
+    {
+        slave = dao.findById(id);
+        return SUCCESS;
+    }
+
+    public String execute()
+    {
+        Slave persistentSlave = dao.findById(id);
+        persistentSlave.setHost(slave.getHost());
+        persistentSlave.setName(slave.getName());
+        persistentSlave.setPort(slave.getPort());
+        dao.save(persistentSlave);
+        
+        return SUCCESS;
+    }
 }
