a60ab9e91c474be24ccf2e16805393910a314cb5
Daniel Ostermeier <daniel@zutubi.com>
2006-02-21 01:25:29 +0000

    CIB-99: checkin work flow for smtp configuration.

---
+++ master/src/java/com/cinnamonbob/web/admin/SmtpAction.java
@@ -0,0 +1,14 @@
+package com.cinnamonbob.web.admin;
+
+import com.cinnamonbob.web.ActionSupport;
+
+/**
+ * <class-comment/>
+ */
+public class SmtpAction extends ActionSupport
+{
+    public String doSave()
+    {
+        return SUCCESS;
+    }
+}
