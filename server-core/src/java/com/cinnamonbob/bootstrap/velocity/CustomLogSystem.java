a251eee072e0903e5cb4654d96e25ef68c3adb74
Jason Sankey <jason@zutubi.com>
2006-02-28 23:53:20 +0000

    CIB-27: Added a custom velocity logger that connects velocity logging to the rest of our logging.

---
+++ server-core/src/java/com/cinnamonbob/bootstrap/velocity/CustomLogSystem.java
@@ -0,0 +1,39 @@
+package com.cinnamonbob.bootstrap.velocity;
+
+import com.cinnamonbob.util.logging.Logger;
+import org.apache.velocity.runtime.RuntimeServices;
+import org.apache.velocity.runtime.log.LogSystem;
+
+/**
+ */
+public class CustomLogSystem implements LogSystem
+{
+    private static final Logger LOG = Logger.getLogger(CustomLogSystem.class);
+
+    public void init(RuntimeServices runtimeServices) throws Exception
+    {
+        // Do nothing
+    }
+
+    public void logVelocityMessage(int level, String message)
+    {
+        switch (level)
+        {
+            case DEBUG_ID:
+                LOG.debug(message);
+                break;
+            case INFO_ID:
+                LOG.info(message);
+                break;
+            case WARN_ID:
+                LOG.warning(message);
+                break;
+            case ERROR_ID:
+                LOG.error(message);
+                break;
+            default:
+                LOG.fine(message);
+                break;
+        }
+    }
+}
