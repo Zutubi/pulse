991719ed00909e6f6724030d70ea25117bb11a44
Daniel Ostermeier <daniel@zutubi.com>
2006-03-03 08:30:18 +0000

    This class is created by xwork and hence can be autowired.

--- server-core/src/java/com/cinnamonbob/velocity/CustomVelocityManager.java
+++ server-core/src/java/com/cinnamonbob/velocity/CustomVelocityManager.java
@@ -2,6 +2,7 @@ package com.cinnamonbob.velocity;
 
 import com.cinnamonbob.bootstrap.ApplicationConfiguration;
 import com.cinnamonbob.bootstrap.ConfigUtils;
+import com.cinnamonbob.bootstrap.ConfigurationManager;
 import com.opensymphony.webwork.views.velocity.VelocityManager;
 import com.opensymphony.xwork.util.OgnlValueStack;
 import org.apache.velocity.context.Context;
@@ -13,11 +14,18 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class CustomVelocityManager extends VelocityManager
 {
+    private ConfigurationManager configManager;
+
     public Context createContext(OgnlValueStack stack, HttpServletRequest req, HttpServletResponse res)
     {
         Context context = super.createContext(stack, req, res);
-        ApplicationConfiguration config = ConfigUtils.getManager().getAppConfig();
+        ApplicationConfiguration config = configManager.getAppConfig();
         context.put("helpUrl", config.getHelpUrl());
         return context;
+    }
+
+    public void setConfigurationManager(ConfigurationManager configManager)
+    {
+        this.configManager = configManager;
     }
 }
