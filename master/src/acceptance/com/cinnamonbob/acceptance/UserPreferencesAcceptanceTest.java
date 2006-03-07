1925f2a47d9b84c6c7d1f8cb423493f6087b1ee8
Daniel Ostermeier <daniel@zutubi.com>
2006-03-07 04:47:49 +0000

    Added some validation for the user preferences create contact process.

--- master/src/acceptance/com/cinnamonbob/acceptance/UserPreferencesAcceptanceTest.java
+++ master/src/acceptance/com/cinnamonbob/acceptance/UserPreferencesAcceptanceTest.java
@@ -9,6 +9,12 @@ import com.cinnamonbob.core.util.RandomUtils;
 public class UserPreferencesAcceptanceTest extends BaseAcceptanceTest
 {
     private String login;
+    private static final String CONTACT_CREATE = "contact.create";
+    private static final String CONTACT_CREATE_TYPE = "contact";
+    private static final String EMAIL_CREATE_NAME = "contact.name";
+    private static final String EMAIL_CREATE = "email.create";
+    private static final String EMAIL_CREATE_EMAIL = "contact.email";
+    private static final String CREATE_CONTACT_LINK = "create contact";
 
     public UserPreferencesAcceptanceTest()
     {
@@ -40,9 +46,7 @@ public class UserPreferencesAcceptanceTest extends BaseAcceptanceTest
     public void testUserPreferences()
     {
         // navigate to the preferences tab.
-        gotoPage("/");
-        clickLinkWithText("dashboard");
-        clickLinkWithText("preferences");
+        navigateToPreferences();
 
         // assert tabular data.
         assertTablePresent("user");
@@ -51,7 +55,105 @@ public class UserPreferencesAcceptanceTest extends BaseAcceptanceTest
                 new String[]{"name", login}     // name row
         });
 
-        assertLinkPresentWithText("create contact");
+        assertTablePresent("contacts");
+        assertTableRowsEqual("contacts", 1, new String[][]{
+                new String[]{"name", "uid", "actions"},
+                new String[]{CREATE_CONTACT_LINK, CREATE_CONTACT_LINK, CREATE_CONTACT_LINK}
+        });
+
+        // test creation of a contact point.
+        assertLinkPresentWithText(CREATE_CONTACT_LINK);
+
+        clickLinkWithText(CREATE_CONTACT_LINK);
+        assertFormPresent(CONTACT_CREATE);
+
+        setWorkingForm(CONTACT_CREATE);
+        setFormElement(CONTACT_CREATE_TYPE, "email");
+        submit("next");
+
+        assertFormPresent(EMAIL_CREATE);
+        setWorkingForm(EMAIL_CREATE);
+        setFormElement(EMAIL_CREATE_NAME, "home");
+        setFormElement(EMAIL_CREATE_EMAIL, "user@example.com");
+        submit("next");
+
+        // assert that the contact appears as expected.
+        assertTablePresent("contacts");
+        assertTableRowsEqual("contacts", 1, new String[][]{
+                new String[]{"name", "uid", "actions", "actions"},               // header row
+                new String[]{"home", "user@example.com", "edit", "delete"},     // name row
+                new String[]{CREATE_CONTACT_LINK, CREATE_CONTACT_LINK, CREATE_CONTACT_LINK, CREATE_CONTACT_LINK}
+        });
+
+
+        assertTablePresent("subscriptions");
+        assertTableRowsEqual("subscriptions", 1, new String[][]{
+                new String[]{"project", CONTACT_CREATE_TYPE, "condition", "actions"},
+                new String[]{"create subscription", "create subscription", "create subscription", "create subscription"}
+        });
+
+
         assertLinkPresentWithText("create subscription");
+
+        // can not create subscriptions unless there are projects to subscribe to.
     }
+
+    public void testContactFormValidation()
+    {
+        navigateToPreferences();
+
+        clickLinkWithText(CREATE_CONTACT_LINK);
+
+        assertFormPresent(CONTACT_CREATE);
+        setWorkingForm(CONTACT_CREATE);
+        setFormElement(CONTACT_CREATE_TYPE, "email");
+        submit("next");
+
+        assertFormPresent(EMAIL_CREATE);
+        setWorkingForm(EMAIL_CREATE);
+        setFormElement(EMAIL_CREATE_NAME, "");
+        setFormElement(EMAIL_CREATE_EMAIL, "email@example.com");
+        submit("next");
+
+        assertTextPresent("required");
+        assertFormPresent(EMAIL_CREATE);
+        assertFormElementEquals(EMAIL_CREATE_NAME, "");
+        assertFormElementEquals(EMAIL_CREATE_EMAIL, "email@example.com");
+
+        setWorkingForm(EMAIL_CREATE);
+        setFormElement(EMAIL_CREATE_NAME, "example");
+        setFormElement(EMAIL_CREATE_EMAIL, "");
+        submit("next");
+
+        assertTextPresent("required");
+        assertFormPresent(EMAIL_CREATE);
+        assertFormElementEquals(EMAIL_CREATE_NAME, "example");
+        assertFormElementEquals(EMAIL_CREATE_EMAIL, "");
+
+        setWorkingForm(EMAIL_CREATE);
+        setFormElement(EMAIL_CREATE_NAME, "example");
+        setFormElement(EMAIL_CREATE_EMAIL, "incorrect email address");
+        submit("next");
+
+        assertTextPresent("valid");
+        assertFormPresent(EMAIL_CREATE);
+        assertFormElementEquals(EMAIL_CREATE_NAME, "example");
+        assertFormElementEquals(EMAIL_CREATE_EMAIL, "incorrect email address");
+    }
+
+    public void testSubscriptionFormValidation()
+    {
+        navigateToPreferences();
+
+        // can not create subscription without project
+        // can not create subscription without contacts.
+    }
+
+    private void navigateToPreferences()
+    {
+        gotoPage("/");
+        clickLinkWithText("dashboard");
+        clickLinkWithText("preferences");
+    }
+
 }
