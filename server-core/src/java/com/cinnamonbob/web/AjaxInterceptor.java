383cc938b4eb9f08890c4a26583424ab365cdd85
Jason Sankey <jason@zutubi.com>
2006-03-27 00:03:17 +0000

    CIB-224: autorefreshing pages: added ptototype and refreshing for dashboard.  More to do.  UI tweaks: dropping some actions, shrinking wasted space at top of page.  Tweaks for test reports, and inclusion of test report view on summary page (rather than custom list).

---
+++ server-core/src/java/com/cinnamonbob/web/AjaxInterceptor.java
@@ -0,0 +1,24 @@
+package com.cinnamonbob.web;
+
+import com.opensymphony.webwork.ServletActionContext;
+import com.opensymphony.xwork.ActionInvocation;
+import com.opensymphony.xwork.interceptor.AroundInterceptor;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ */
+public class AjaxInterceptor extends AroundInterceptor
+{
+    protected void after(ActionInvocation actionInvocation, String string) throws Exception
+    {
+    }
+
+    protected void before(ActionInvocation actionInvocation) throws Exception
+    {
+        HttpServletResponse response = ServletActionContext.getResponse();
+
+        response.setHeader("Cache-Control", "must-revalidate");
+        response.setDateHeader("Expires", 0);
+    }
+}
