59f51f668a1acceff4eb8534fe9d4644328879e1
Daniel Ostermeier <daniel@zutubi.com>
2006-05-09 03:39:20 +0000

    CIB-333: check box with box on the left and label on the right. Available via #wwccheckbox. Standard checkbox still available via #wwcheckbox

---
+++ server-core/src/java/com/zutubi/pulse/webwork/components/CustomCheckbox.java
@@ -0,0 +1,26 @@
+/********************************************************************************
+ @COPYRIGHT@
+ ********************************************************************************/
+package com.zutubi.pulse.webwork.components;
+
+import com.opensymphony.webwork.components.Checkbox;
+import com.opensymphony.xwork.util.OgnlValueStack;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <class-comment/>
+ */
+public class CustomCheckbox extends Checkbox
+{
+    public CustomCheckbox(OgnlValueStack stack, HttpServletRequest request, HttpServletResponse response)
+    {
+        super(stack, request, response);
+    }
+
+    protected String getDefaultTemplate()
+    {
+        return "ccheckbox";
+    }
+}
---
+++ server-core/src/java/com/zutubi/pulse/webwork/velocity/CustomCheckboxDirective.java
@@ -0,0 +1,33 @@
+/********************************************************************************
+ @COPYRIGHT@
+ ********************************************************************************/
+package com.zutubi.pulse.webwork.velocity;
+
+import com.opensymphony.webwork.views.velocity.components.AbstractDirective;
+import com.opensymphony.webwork.components.Component;
+import com.opensymphony.xwork.util.OgnlValueStack;
+import com.zutubi.pulse.webwork.components.CustomCheckbox;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <class-comment/>
+ */
+public class CustomCheckboxDirective extends AbstractDirective
+{
+    public String getBeanName()
+    {
+        return "ccheckbox";
+    }
+
+    protected Component getBean(OgnlValueStack stack, HttpServletRequest req, HttpServletResponse res)
+    {
+        return new CustomCheckbox(stack, req, res);
+    }
+
+    public int getType()
+    {
+        return LINE;
+    }
+}
