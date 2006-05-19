27367e1f1ef8209286c3ff2385d77982f3527aae
Jason Sankey <jason@zutubi.com>
2006-05-19 05:37:50 +0000

    CIB-394: expand/collapse artifact directories.  Also made clicking on the text expand/collapse the element.

--- master/src/www/main.js
+++ master/src/www/main.js
@@ -98,6 +98,10 @@ function toggleDisplay(id)
 }
 
 // Toggles the display of a nested list and switched the correcsponding image
+//   - if a second argument is given, it is used as the image for a "closed"
+//     element
+//   - if a third argument is given, it is used as the image for a "open"
+//     element
 function toggleList(id)
 {
     var element = getElement(id);
@@ -105,14 +109,31 @@ function toggleList(id)
 
     if(!element.style.display)
     {
+        var closedImage = '/images/resultset_next.gif';
+        if(arguments.length > 1)
+        {
+            closedImage = arguments[1];
+        }
+
         element.style.display = 'none';
-        image.src = '/images/resultset_next.gif';
+        image.src = closedImage;
     }
     else
     {
+        var openImage = '/images/resultset_down.gif';
+        if(arguments.length > 2)
+        {
+            openImage = arguments[2];
+        }
+
         element.style.display = '';
-        image.src = '/images/resultset_down.gif';
+        image.src = openImage;
     }
+}
+
+function toggleFolderList(id)
+{
+    toggleList(id, 'images/tree/foldericon.png', 'images/tree/openfoldericon.png');
 }
 
 // Changes display style for all lists under the given node
@@ -129,7 +150,7 @@ function styleAllLists(id, style, imageSource)
             var image = getElement(childLists[i].id + '_image');
             if(image)
             {
-                image.src = imageSource;
+                image.src = imageSource(childLists[i]);
             }
         }
     }
@@ -138,13 +159,13 @@ function styleAllLists(id, style, imageSource)
 // Expands all lists under the given node
 function expandAllLists(id)
 {
-    styleAllLists(id, '', '/images/resultset_down.gif');
+    styleAllLists(id, '', function(element) { if(element.parentNode.className == "dir-artifact") { return '/images/tree/openfoldericon.png'; } else { return '/images/resultset_down.gif'; } });
 }
 
 // Collapses all lists under the given node
 function collapseAllLists(id)
 {
-    styleAllLists(id, 'none', '/images/resultset_next.gif');
+    styleAllLists(id, 'none', function(element) { if(element.parentNode.className == "dir-artifact") { return '/images/tree/foldericon.png'; } else { return '/images/resultset_next.gif'; } });
 }
 
 // Hides all children of the element with the given id
