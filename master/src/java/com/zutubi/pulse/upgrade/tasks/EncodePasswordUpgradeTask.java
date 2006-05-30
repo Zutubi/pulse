06faa750fa12bca3cc631afc79c968722415f11b
Daniel Ostermeier <daniel@zutubi.com>
2006-05-30 13:22:31 +0000

    CIB-409: cloned scms are shared by multiple projects. Added upgrade task to fix the data.

--- master/src/java/com/zutubi/pulse/upgrade/tasks/EncodePasswordUpgradeTask.java
+++ master/src/java/com/zutubi/pulse/upgrade/tasks/EncodePasswordUpgradeTask.java
@@ -3,15 +3,11 @@
  ********************************************************************************/
 package com.zutubi.pulse.upgrade.tasks;
 
-import com.zutubi.pulse.upgrade.DataSourceAware;
 import com.zutubi.pulse.upgrade.UpgradeContext;
-import com.zutubi.pulse.upgrade.UpgradeException;
-import com.zutubi.pulse.upgrade.UpgradeTask;
 import com.zutubi.pulse.util.JDBCUtils;
 import org.acegisecurity.providers.encoding.Md5PasswordEncoder;
 import org.acegisecurity.providers.encoding.PasswordEncoder;
 
-import javax.sql.DataSource;
 import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,14 +15,8 @@ import java.util.List;
 /**
  * <class-comment/>
  */
-public class EncodePasswordUpgradeTask implements UpgradeTask, DataSourceAware
+public class EncodePasswordUpgradeTask extends DatabaseUpgradeTask
 {
-    private DataSource dataSource;
-
-    private int buildNumber;
-
-    private List<String> errors = new LinkedList<String>();
-
     public String getName()
     {
         return "Encrypt user credentials";
@@ -38,103 +28,64 @@ public class EncodePasswordUpgradeTask implements UpgradeTask, DataSourceAware
                 "now encrypted in the database.";
     }
 
-    public int getBuildNumber()
+    public void execute(UpgradeContext context, Connection con) throws SQLException
     {
-        return buildNumber;
-    }
+        List<IdPassword> results = new LinkedList<IdPassword>();
 
-    public void setBuildNumber(int buildNumber)
-    {
-        this.buildNumber = buildNumber;
-    }
-
-    public void execute(UpgradeContext context) throws UpgradeException
-    {
-        Connection con = null;
+        // lookup all of the users, retrieving the id and password fields.
+        CallableStatement stmt = null;
+        ResultSet rs = null;
         try
         {
-            con = dataSource.getConnection();
-
-            List<IdPassword> results = new LinkedList<IdPassword>();
-
-            // lookup all of the users, retrieving the id and password fields.
-            CallableStatement stmt = null;
-            ResultSet rs = null;
-            try
+            stmt = con.prepareCall("SELECT id, password FROM user");
+            rs = stmt.executeQuery();
+            while (rs.next())
             {
-                stmt = con.prepareCall("SELECT id, password FROM user");
-                rs = stmt.executeQuery();
-                while (rs.next())
-                {
-                    results.add(new IdPassword(JDBCUtils.getLong(rs, "id"), JDBCUtils.getString(rs, "password")));
-                }
+                results.add(new IdPassword(JDBCUtils.getLong(rs, "id"), JDBCUtils.getString(rs, "password")));
             }
-            finally
-            {
-                JDBCUtils.close(rs);
-                JDBCUtils.close(stmt);
-            }
-
-            // update the passwords.
-            PasswordEncoder encoder = new Md5PasswordEncoder();
-
-            for (IdPassword data : results)
-            {
-                data.password = encoder.encodePassword(data.password, null);
-            }
-
-            // update the database.
-            PreparedStatement ps = null;
-            try
-            {
-                ps = con.prepareStatement("UPDATE user SET password = ? WHERE id = ?");
-                for (IdPassword data : results)
-                {
-                    JDBCUtils.setString(ps, 1, data.password);
-                    JDBCUtils.setLong(ps, 2, data.id);
-
-
-                    int rowCount = ps.executeUpdate();
-                    if (rowCount != 1)
-                    {
-                        errors.add("Failed to update password for user " + data.id +
-                                ". Row count is " + rowCount + " where 1 was expected.");
-                    }
-                }
-            }
-            finally
-            {
-                JDBCUtils.close(ps);
-            }
-        }
-        catch (SQLException e)
-        {
-            errors.add("SQLException: " + e.getMessage());
         }
         finally
         {
-            JDBCUtils.close(con);
+            JDBCUtils.close(rs);
+            JDBCUtils.close(stmt);
         }
-    }
 
-    public List<String> getErrors()
-    {
-        return errors;
+        // update the passwords.
+        PasswordEncoder encoder = new Md5PasswordEncoder();
+
+        for (IdPassword data : results)
+        {
+            data.password = encoder.encodePassword(data.password, null);
+        }
+
+        // update the database.
+        PreparedStatement ps = null;
+        try
+        {
+            ps = con.prepareStatement("UPDATE user SET password = ? WHERE id = ?");
+            for (IdPassword data : results)
+            {
+                JDBCUtils.setString(ps, 1, data.password);
+                JDBCUtils.setLong(ps, 2, data.id);
+
+
+                int rowCount = ps.executeUpdate();
+                if (rowCount != 1)
+                {
+                    errors.add("Failed to update password for user " + data.id +
+                            ". Row count is " + rowCount + " where 1 was expected.");
+                }
+            }
+        }
+        finally
+        {
+            JDBCUtils.close(ps);
+        }
     }
 
     public boolean haltOnFailure()
     {
         return false;
-    }
-
-    /**
-     * Required resource.
-     *
-     * @param source
-     */
-    public void setDataSource(DataSource source)
-    {
-        this.dataSource = source;
     }
 
     private class IdPassword
---
+++ master/src/java/com/zutubi/pulse/upgrade/tasks/HibernateUtils.java
@@ -0,0 +1,63 @@
+/********************************************************************************
+ @COPYRIGHT@
+ ********************************************************************************/
+package com.zutubi.pulse.upgrade.tasks;
+
+import com.zutubi.pulse.util.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * <class-comment/>
+ */
+public class HibernateUtils
+{
+    /**
+     * Emulate hibernates hilo key generation.
+     *
+     * @param con
+     *
+     * @return
+     *
+     * @throws SQLException
+     */
+    public static long getNextId(Connection con) throws SQLException
+    {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        long nextId;
+
+        try
+        {
+            ps = con.prepareStatement("select NEXT_HI from HIBERNATE_UNIQUE_KEY");
+            rs = ps.executeQuery();
+            rs.next();
+            nextId = rs.getLong(1);
+        }
+        finally
+        {
+            JDBCUtils.close(ps);
+            JDBCUtils.close(rs);
+        }
+
+        try
+        {
+            nextId++;
+            ps = con.prepareStatement("UPDATE hibernate_unique_key SET next_hi = ? where next_hi = ?");
+            JDBCUtils.setLong(ps, 1, nextId);
+            JDBCUtils.setLong(ps, 2, (nextId - 1));
+            ps.executeUpdate();
+        }
+        finally
+        {
+            JDBCUtils.close(ps);
+        }
+
+        // Emulate Hibernate's hilo algorithm
+        return nextId * (Short.MAX_VALUE + 1) + 1;
+    }
+
+}
