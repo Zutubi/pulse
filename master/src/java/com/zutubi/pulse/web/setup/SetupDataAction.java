073ef65a267b761f32df635260bba89157be1de2
Daniel Ostermeier <daniel@zutubi.com>
2006-07-01 13:47:26 +0000

    Patch SetupDataAction. Do not trigger a data.init when using a pre initialised data directory

--- master/src/java/com/zutubi/pulse/web/setup/SetupDataAction.java
+++ master/src/java/com/zutubi/pulse/web/setup/SetupDataAction.java
@@ -3,6 +3,7 @@ package com.zutubi.pulse.web.setup;
 import com.zutubi.pulse.bootstrap.MasterConfigurationManager;
 import com.zutubi.pulse.bootstrap.SimpleMasterConfigurationManager;
 import com.zutubi.pulse.bootstrap.SetupManager;
+import com.zutubi.pulse.bootstrap.Data;
 
 import java.io.File;
 
@@ -60,7 +61,16 @@ public class SetupDataAction extends SetupActionSupport
     {
         File home = new File(this.data);
         configurationManager.setPulseData(home);
-        configurationManager.getData().init();
+
+        // If this is the first time this directory is being used as a data directory, then we need
+        // to ensure that it is initialised. If we are working with an already existing directory,
+        // then it will have been initialised and no re-initialisation is required (or allowed).
+        Data d = configurationManager.getData();
+        if (!d.isInitialised())
+        {
+            configurationManager.getData().init();
+        }
+
         setupManager.requestDataComplete();
         
         return SUCCESS;
