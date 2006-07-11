b899217b552b92185f41f8e472ef96a6259df7bc
Daniel Ostermeier <daniel@zutubi.com>
2006-07-11 13:18:54 +0000

    CIB-447: introduce simple namespacing to try and keep the javascript namespace under control. - fix problem with browse-project-dir page attempting to load a tree when no tree element is available.

---
+++ master/src/www/js/widget/toolbar.js
@@ -0,0 +1,264 @@
+ZUTUBI.widget.Toolbar = function(id)
+{
+    this.initialize(id);
+}
+
+ZUTUBI.widget.Toolbar.prototype = {
+
+    initialize: function(id)
+    {
+        this.id = id;
+        this.tools = [];
+    },
+
+    getToolByIndex: function(index)
+    {
+        return this.tools[index];
+    },
+
+    add: function(tool)
+    {
+        this.tools[this.tools.length] = tool;
+    },
+
+    draw: function()
+    {
+        var html = this.getHtml();
+        document.getElementById(this.id).innerHTML = html;
+    },
+
+    getHtml: function()
+    {
+        var sb = [];
+
+        sb[sb.length] = '<table border="0" cellpadding="0" cellspacing="0">';
+        sb[sb.length] = '<tr>';
+
+        // draw icons.
+        $A(this.tools).each(function(toolbarItem)
+        {
+            sb[sb.length] = toolbarItem.getHtml();
+        });
+
+        sb[sb.length] = '</tr>';
+        sb[sb.length] = '</table>';
+
+        return sb.join("");
+    }
+};
+
+/**
+ *
+ *
+ */
+ZUTUBI.widget.ToolbarItem = function(id)
+{
+    this.initialize(id);
+}
+
+ZUTUBI.widget.ToolbarItem.itemCount = 0;
+ZUTUBI.widget.ToolbarItem.items = [];
+
+ZUTUBI.widget.ToolbarItem.prototype = {
+
+    initialize: function(id)
+    {
+        this.id = id;
+        this.index = ZUTUBI.widget.ToolbarItem.itemCount;
+        ZUTUBI.widget.ToolbarItem.items[this.index] = this;
+        ZUTUBI.widget.ToolbarItem.itemCount++;
+
+        this.tooltip = null;
+    },
+
+    onClick: function(me)
+    {
+    },
+
+    /**
+     * Set the tooltip message.
+     */
+    setTooltip: function(tooltip)
+    {
+        this.tooltip = tooltip;
+    },
+
+    /**
+     * Get the tooltip message.
+     */
+    getTooltip: function()
+    {
+        return this.tooltip;
+    },
+
+    getToolElId: function()
+    {
+        return "ztbt" + this.index;
+    },
+
+    getToolEl: function()
+    {
+        return document.getElementById(this.getToolElId());
+    },
+
+    getToolStyle: function()
+    {
+        return this.id + " toolbaritem";
+    },
+
+    onMouseOver: function(event)
+    {
+        Element.addClassName(this.getToolEl(), 'selected');
+
+        // show the tooltip if one is configured.
+        var tooltip = ZUTUBI.widget.TooltipFactory.getTooltip();
+        if (this.tooltip)
+        {
+            tooltip.setTip(this.tooltip);
+            tooltip.show();
+        }
+    },
+
+    onMouseOut: function(event)
+    {
+        Element.removeClassName(this.getToolEl(), 'selected');
+
+        // hide the tooltip if one is visible.
+        var tooltip = ZUTUBI.widget.TooltipFactory.getTooltip();
+        tooltip.hide();
+    },
+
+    getHtml: function()
+    {
+        var getTool = 'ZUTUBI.widget.ToolbarItem.items[\'' + this.index + '\']';
+
+        var sb = [];
+        sb[sb.length] = '<td';
+        sb[sb.length] = ' id="' + this.getToolElId() + '"';
+        sb[sb.length] = ' class="' + this.getToolStyle() + '"';
+        sb[sb.length] = ' onclick="javascript:' + getTool + '.onClick('+getTool+');"';
+        sb[sb.length] = ' onmouseover="javascript:' + getTool + '.onMouseOver('+getTool+');" ';
+        sb[sb.length] = ' onmouseout="javascript:' + getTool + '.onMouseOut('+getTool+');" ';
+        sb[sb.length] = '>';
+        sb[sb.length] = '</td>';
+        return sb.join("");
+    }
+};
+
+/**
+ * A toolbar separator is an inactive component in a toolbar used as a
+ * separator between groups of related toolbar items.
+ *
+ * The toolbar separator does not reactive to mouse or lick events.
+ */
+ZUTUBI.widget.ToolbarSeparator = function()
+{
+    this.initialize();
+}
+
+ZUTUBI.widget.ToolbarSeparator.prototype = {
+
+    initialize: function()
+    {
+
+    },
+
+    getToolStyle: function()
+    {
+        return "toolbarseparator";
+    },
+
+    getHtml: function()
+    {
+        var sb = [];
+        sb[sb.length] = '<td';
+        sb[sb.length] = ' class="' + this.getToolStyle() + '"';
+        sb[sb.length] = '>';
+        sb[sb.length] = '</td>';
+        return sb.join("");
+    }
+};
+
+
+/**
+ * Tooltip for the toolbar items.
+ *
+ */
+ZUTUBI.widget.TooltipFactory = {};
+
+ZUTUBI.widget.TooltipFactory._tip = null;
+
+ZUTUBI.widget.TooltipFactory.getTooltip = function()
+{
+    if (!ZUTUBI.widget.TooltipFactory._tip)
+    {
+        var tip = new ZUTUBI.widget.Tooltip('tooltip');
+        ZUTUBI.widget.TooltipFactory._tip = tip;
+        Event.observe(document, "mousemove", tip.monitorMouseMovement.bindAsEventListener(tip), false);
+    }
+    return ZUTUBI.widget.TooltipFactory._tip;
+};
+
+/**
+ * Tooltip.
+ *
+ */
+ZUTUBI.widget.Tooltip = function(id)
+{
+    this.initialize(id);
+};
+
+ZUTUBI.widget.Tooltip.prototype = {
+
+    initialize: function(id)
+    {
+        this.id = id;
+    },
+
+    getTooltipEl: function()
+    {
+        var el = document.getElementById(this.id);
+        if (!el)
+        {
+            // create it if it does not already exist.
+            el = document.createElement('div');
+            el.id = this.id;
+            with(el.style)
+            {
+                display = 'none';
+                position = 'absolute';
+            }
+            el.innerHTML = '&nbsp;';
+            document.body.appendChild(el);
+        }
+        return el;
+    },
+
+    setTip: function(msg)
+    {
+        this.getTooltipEl().innerHTML = msg;
+    },
+
+    show: function()
+    {
+        Element.show(this.getTooltipEl());
+    },
+
+    hide: function()
+    {
+        Element.hide(this.getTooltipEl());
+    },
+
+    monitorMouseMovement: function(event)
+    {
+        var mLoc = {"x":Event.pointerX(event), "y":Event.pointerY(event)};
+        // todo: this should be the window offset so this works when the window scrollbars are out.
+        var wLoc = {"x":0, "y":0};
+        var offset = {"x":12, "y":8};
+
+        var x = mLoc.x + wLoc.x + offset.x;
+        var y = mLoc.y + wLoc.y + offset.y;
+
+        Element.setStyle(this.getTooltipEl(), {"left":x + "px", "top":y + "px"})
+    }
+};
