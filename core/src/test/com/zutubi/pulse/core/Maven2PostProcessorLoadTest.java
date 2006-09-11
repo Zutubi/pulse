9742e979ac5ee22a99456eb44c28f65d8a15fa46
Jason Sankey <jason@zutubi.com>
2006-09-11 08:29:22 +0000

    CIB-616: Improve maven 2 error detection on windows.

--- core/src/test/com/zutubi/pulse/core/Maven2PostProcessorLoadTest.java
+++ core/src/test/com/zutubi/pulse/core/Maven2PostProcessorLoadTest.java
@@ -7,6 +7,14 @@ public class Maven2PostProcessorLoadTest extends FileLoaderTestBase
     public void testEmpty() throws PulseException
     {
         Maven2PostProcessor pp = referenceHelper("empty");
-        assertEquals(2, pp.size());
+        assertEquals(3, pp.size());
+    }
+
+    public void testFailOnError() throws PulseException
+    {
+        Maven2PostProcessor pp = referenceHelper("fail");
+        assertEquals(3, pp.size());
+        assertTrue(((RegexPostProcessor)pp.get(1)).getFailOnError());
+        assertTrue(((RegexPostProcessor)pp.get(2)).getFailOnError());
     }
 }
