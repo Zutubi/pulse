28dd0785cb782f3ff64988c32217fbbe1da2919a
Daniel Ostermeier <daniel@zutubi.com>
2006-10-03 09:54:26 +0000

    CIB-404: refactor of the BuildResutlsRssAction to simplify the addition of extra feeds configurations.

--- master/src/java/com/zutubi/pulse/web/rss/BuildResultsRssAction.java
+++ master/src/java/com/zutubi/pulse/web/rss/BuildResultsRssAction.java
@@ -17,6 +17,7 @@ import java.io.StringWriter;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Arrays;
 
 /**
  * <class-comment/>
@@ -47,11 +48,11 @@ public class BuildResultsRssAction extends ProjectActionSupport
         Project project = getProject();
         if (project == null)
         {
-            generatePulseBuildResultsFeed();
+            feed = generateFeed(new AllProjectsResultTemplate());
         }
         else
         {
-            generateProjectBuildResultsFeed(project);
+            feed = generateFeed(new ProjectResultTemplate(project));
         }
 
         // return the requested feed type. at the moment,
@@ -59,126 +60,44 @@ public class BuildResultsRssAction extends ProjectActionSupport
         return "rss";
     }
 
-    private void generateProjectBuildResultsFeed(Project project)
+    private SyndFeedImpl generateFeed(RssFeedTemplate template)
     {
-        // define the query to match the results returned by project build result rss feed.
-        SearchQuery<BuildResult> query = queries.getBuildResults();
-        query.add(BuildResultExpressions.projectEq(project));
-        query.add(BuildResultExpressions.buildResultCompleted());
-        query.setFirstResult(0);
-        query.setMaxResults(10);
-        query.add(Order.desc("number"));
-
         // build the rss feed.
-        feed = new SyndFeedImpl();
+        SyndFeedImpl feed = new SyndFeedImpl();
 
         // set Title, Description and Link
-        feed.setTitle("Pulse build results for " + project.getName());
-        feed.setDescription("This feed contains the latest pulse build results for the " + project.getName() + " project.");
-        feed.setLink(configurationManager.getAppConfig().getBaseUrl() +"/currentBuild.action?id=" + project.getId());
+        feed.setTitle(template.getTitle());
+        feed.setDescription(template.getDescription());
+        feed.setLink(template.getLink());
 
         List<SyndEntry> entries = new LinkedList<SyndEntry>();
-        for (BuildResult result : query.list())
+        for (BuildResult result : template.getQuery().list())
         {
             SyndEntry entry = new SyndEntryImpl();
 
             // with rss 2.0, the content is added in the description field.
             SyndContent description = new SyndContentImpl();
 
-            StringBuffer titleBuffer = new StringBuffer();
-            titleBuffer.append("Build ");
-            titleBuffer.append(result.getNumber());
-            titleBuffer.append(" ");
-            titleBuffer.append(result.succeeded() ? "succeeded" : "failed");
-
             // type should be based on user selected type.
             description.setType("text/plain");
-            description.setValue(titleBuffer.toString());
+            description.setValue(template.getEntryTitle(result));
             entry.setDescription(description);
-
-            entry.setTitle(titleBuffer.toString());
+            entry.setTitle(template.getTitle());
 
             ContentModule content = new ContentModuleImpl();
-            content.setEncodeds(asList(renderResult(result)));
-            entry.setModules(asList(content));
+            content.setEncodeds(Arrays.asList(renderResult(result)));
+            entry.setModules(Arrays.asList(content));
 
             // NOTES:
             // calling setLink is effectively setting guid without a isPermaLink reference.
             // calling setUri() is equivalent to guid isPermaLink=false - refer to ConverterForRSS094.java
-            String permalink = configurationManager.getAppConfig().getBaseUrl() +"/viewBuild.action?id=" + result.getId();
-            entry.setLink(permalink);
+            entry.setLink(template.getEntryLink(result));
             entry.setPublishedDate(new Date(result.getStamps().getEndTime()));
             entries.add(entry);
         }
         feed.setEntries(entries);
 
-
-    }
-
-    private void generatePulseBuildResultsFeed()
-    {
-        // generate the search query.
-        SearchQuery<BuildResult> query = queries.getBuildResults();
-        query.add(BuildResultExpressions.buildResultCompleted());
-        query.setFirstResult(0);
-        query.setMaxResults(10);
-        query.add(Order.desc("number"));
-
-        // build the rss feed.
-        feed = new SyndFeedImpl();
-
-        // set Title, Description and Link
-        feed.setTitle("Pulse build results");
-        feed.setDescription("This feed contains the latest pulse build results.");
-        String baseUrl = configurationManager.getAppConfig().getBaseUrl();
-        feed.setLink(baseUrl +"/viewProjects.action");
-
-        List<SyndEntry> entries = new LinkedList<SyndEntry>();
-        for (BuildResult result : query.list())
-        {
-            SyndEntry entry = new SyndEntryImpl();
-
-            // with rss 2.0, the content is added in the description field.
-            SyndContent description = new SyndContentImpl();
-
-            StringBuffer titleBuffer = new StringBuffer();
-            titleBuffer.append("Project ");
-            titleBuffer.append(result.getProject().getName());
-            titleBuffer.append(" build ");
-            titleBuffer.append(result.getNumber());
-            titleBuffer.append(" ");
-            titleBuffer.append(result.succeeded() ? "succeeded" : "failed");
-
-            // type should be based on user selected type.
-            description.setType("text/plain");
-            description.setValue(titleBuffer.toString());
-            entry.setDescription(description);
-
-            entry.setTitle(titleBuffer.toString());
-
-            ContentModule content = new ContentModuleImpl();
-            content.setEncodeds(asList(renderResult(result)));
-            entry.setModules(asList(content));
-
-            // NOTES:
-            // calling setLink is effectively setting guid without a isPermaLink reference.
-            // calling setUri() is equivalent to guid isPermaLink=false - refer to ConverterForRSS094.java
-            String permalink = baseUrl +"/viewBuild.action?id=" + result.getId();
-            entry.setLink(permalink);
-            entry.setPublishedDate(new Date(result.getStamps().getEndTime()));
-            entries.add(entry);
-        }
-        feed.setEntries(entries);
-    }
-
-    private <X> List<X> asList(X... objs)
-    {
-        List<X> l = new LinkedList<X>();
-        for (X x : objs)
-        {
-            l.add(x);
-        }
-        return l;
+        return feed;
     }
 
     private String renderResult(BuildResult result)
@@ -204,6 +123,146 @@ public class BuildResultsRssAction extends ProjectActionSupport
     public void setQueries(Queries queries)
     {
         this.queries = queries;
+    }
+
+    private interface RssFeedTemplate
+    {
+        SearchQuery<BuildResult> getQuery();
+        String getTitle();
+        String getDescription();
+        String getLink();
+        String getEntryTitle(BuildResult result);
+        String getEntryLink(BuildResult result);
+    }
+
+    private class AllProjectsResultTemplate implements RssFeedTemplate
+    {
+        public SearchQuery<BuildResult> getQuery()
+        {
+            SearchQuery<BuildResult> query = queries.getBuildResults();
+            query.add(BuildResultExpressions.buildResultCompleted());
+            query.setFirstResult(0);
+            query.setMaxResults(10);
+            query.add(Order.desc("number"));
+            return query;
+        }
+
+        public String getTitle()
+        {
+            return "Pulse build results";
+        }
+
+        public String getDescription()
+        {
+            return "This feed contains the latest pulse build results.";
+        }
+
+        public String getLink()
+        {
+            return configurationManager.getAppConfig().getBaseUrl() + "/viewProjects.action";
+        }
+
+        public String getEntryTitle(BuildResult result)
+        {
+            StringBuffer titleBuffer = new StringBuffer();
+            titleBuffer.append("Project ");
+            titleBuffer.append(result.getProject().getName());
+            titleBuffer.append(" build ");
+            titleBuffer.append(result.getNumber());
+            titleBuffer.append(" ");
+            titleBuffer.append(result.succeeded() ? "succeeded" : "failed");
+            return titleBuffer.toString();
+        }
+
+        public String getEntryLink(BuildResult result)
+        {
+            return configurationManager.getAppConfig().getBaseUrl() + "/viewBuild.action?id=" + result.getId();
+        }
+    }
+
+    private class ProjectResultTemplate implements RssFeedTemplate
+    {
+        private Project project;
+
+
+        public ProjectResultTemplate(Project project)
+        {
+            this.project = project;
+        }
+
+        public SearchQuery<BuildResult> getQuery()
+        {
+            SearchQuery<BuildResult> query = queries.getBuildResults();
+            query.add(BuildResultExpressions.projectEq(project));
+            query.add(BuildResultExpressions.buildResultCompleted());
+            query.setFirstResult(0);
+            query.setMaxResults(10);
+            query.add(Order.desc("number"));
+            return query;
+        }
+
+        public String getTitle()
+        {
+            return "Pulse build results for " + project.getName();
+        }
+
+        public String getDescription()
+        {
+            return "This feed contains the latest pulse build results for the " + project.getName() + " project.";
+        }
+
+        public String getLink()
+        {
+            return configurationManager.getAppConfig().getBaseUrl() +"/currentBuild.action?id=" + project.getId();
+        }
+
+        public String getEntryTitle(BuildResult result)
+        {
+            StringBuffer titleBuffer = new StringBuffer();
+            titleBuffer.append("Build ");
+            titleBuffer.append(result.getNumber());
+            titleBuffer.append(" ");
+            titleBuffer.append(result.succeeded() ? "succeeded" : "failed");
+            return titleBuffer.toString();
+        }
+
+        public String getEntryLink(BuildResult result)
+        {
+            return configurationManager.getAppConfig().getBaseUrl() +"/viewBuild.action?id=" + result.getId();
+        }
+    }
+
+    private class UserDashboardTemplate implements RssFeedTemplate
+    {
+        public SearchQuery<BuildResult> getQuery()
+        {
+            return null;
+        }
+
+        public String getTitle()
+        {
+            return null;
+        }
+
+        public String getDescription()
+        {
+            return null;
+        }
+
+        public String getLink()
+        {
+            return null;
+        }
+
+        public String getEntryTitle(BuildResult result)
+        {
+            return null;
+        }
+
+        public String getEntryLink(BuildResult result)
+        {
+            return null;
+        }
     }
 }
 
