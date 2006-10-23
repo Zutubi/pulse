868ffeab669ad6f62d97a6f9f5fdb61afc2e1e13
Daniel Ostermeier <daniel@zutubi.com>
2006-10-23 07:06:56 +0000

    CIB-667:  svn merge -r 2337:2338 svn+ssh://dostermeier@cinnamonbob.com/svnroot/pulse/branches/1.1.x

---
+++ core/src/java/com/zutubi/pulse/core/RegexTestPostProcessor.java
@@ -0,0 +1,176 @@
+package com.zutubi.pulse.core;
+
+import com.zutubi.pulse.core.model.CommandResult;
+import com.zutubi.pulse.core.model.StoredFileArtifact;
+import com.zutubi.pulse.core.model.TestCaseResult;
+import com.zutubi.pulse.core.model.TestSuiteResult;
+import com.zutubi.pulse.util.IOUtils;
+import com.zutubi.pulse.util.logging.Logger;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * <class comment/>
+ */
+public class RegexTestPostProcessor implements PostProcessor
+{
+    private static final Logger LOG = Logger.getLogger(RegexTestPostProcessor.class);
+
+    private String name;
+
+    private BufferedReader reader;
+
+    private String currentLine;
+
+    private String regex;
+    private int statusGroup;
+    private int nameGroup;
+
+    private Map<String, TestCaseResult.Status> statusMap = new HashMap<String, TestCaseResult.Status>();
+
+    public RegexTestPostProcessor()
+    {
+        // provide some defaults.
+        this.statusMap.put("PASS", TestCaseResult.Status.PASS);
+        this.statusMap.put("FAILURE", TestCaseResult.Status.FAILURE);
+        this.statusMap.put("ERROR", TestCaseResult.Status.ERROR);
+    }
+
+    public RegexTestPostProcessor(String name)
+    {
+        this();
+        
+        this.name = name;
+    }
+
+    public void process(StoredFileArtifact artifact, CommandResult result, CommandContext context)
+    {
+        File file = new File(context.getOutputDir(), artifact.getPath());
+        if (!file.isFile())
+        {
+            return;
+        }
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(file));
+
+            // read until you locate the start of a test suite.
+            try
+            {
+                processFile(context.getTestResults());
+            }
+            catch (IllegalStateException e)
+            {
+                // we have come across something we do not understand. Leave the test content
+                // as it is and move on.
+                LOG.info(e);
+            }
+
+        }
+        catch (IOException e)
+        {
+            LOG.info(e);
+        }
+        finally
+        {
+            IOUtils.close(reader);
+        }
+    }
+
+    private void processFile(TestSuiteResult tests) throws IOException
+    {
+        Pattern pattern = Pattern.compile(regex);
+
+        currentLine = nextLine();
+        while (currentLine != null)
+        {
+            Matcher m = pattern.matcher(currentLine);
+            if (m.matches())
+            {
+                String statusString = m.group(statusGroup);
+                String testName = m.group(nameGroup);
+                
+                TestCaseResult result = new TestCaseResult();
+                result.setName(testName);
+                result.setStatus(statusMap.get(statusString));
+
+                tests.add(result);
+            }
+            currentLine = nextLine();
+        }
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Object getValue()
+    {
+        return this;
+    }
+
+    private String nextLine() throws IOException
+    {
+        currentLine = reader.readLine();
+        return currentLine;
+    }
+
+    public void setRegex(String regex)
+    {
+        this.regex = regex;
+    }
+
+    public String getRegex()
+    {
+        return regex;
+    }
+
+    public void setStatusGroup(int i)
+    {
+        this.statusGroup = i;
+    }
+
+    public int getStatusGroup()
+    {
+        return statusGroup;
+    }
+
+    public void setNameGroup(int i)
+    {
+        this.nameGroup = i;
+    }
+
+    public int getNameGroup()
+    {
+        return nameGroup;
+    }
+
+    public void setPassStatus(String status)
+    {
+        this.statusMap.put(status, TestCaseResult.Status.PASS);
+    }
+
+    public void setFailureStatus(String status)
+    {
+        this.statusMap.put(status, TestCaseResult.Status.FAILURE);
+    }
+
+    public void addText(String txt)
+    {
+        regex = txt;
+    }
+}
---
+++ core/src/test/com/zutubi/pulse/core/RegexTestPostProcessorTest.java
@@ -0,0 +1,85 @@
+package com.zutubi.pulse.core;
+
+import com.zutubi.pulse.test.PulseTestCase;
+import com.zutubi.pulse.core.model.StoredFileArtifact;
+import com.zutubi.pulse.core.model.CommandResult;
+import com.zutubi.pulse.core.model.TestSuiteResult;
+import com.zutubi.pulse.util.FileSystemUtils;
+import com.zutubi.pulse.util.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileOutputStream;
+
+/**
+ * <class comment/>
+ */
+public class RegexTestPostProcessorTest extends PulseTestCase
+{
+    private File tmpDir = null;
+    private StoredFileArtifact artifact = null;
+    private CommandResult result = null;
+
+    public RegexTestPostProcessorTest()
+    {
+    }
+
+    public RegexTestPostProcessorTest(String name)
+    {
+        super(name);
+    }
+
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+
+        tmpDir = FileSystemUtils.createTempDirectory("RegexTestPostProcessorTest", getName());
+
+        artifact = prepareArtifact(this.getName());
+
+        result = new CommandResult("output");
+    }
+
+    protected void tearDown() throws Exception
+    {
+        artifact = null;
+        removeDirectory(tmpDir);
+
+        super.tearDown();
+    }
+
+    private StoredFileArtifact prepareArtifact(String name) throws IOException
+    {
+        File tmpFile = new File(tmpDir, name + ".txt");
+        IOUtils.joinStreams(
+                this.getClass().getResourceAsStream("RegexTestPostProcessorTest."+name+".txt"),
+                new FileOutputStream(tmpFile),
+                true
+        );
+
+        return new StoredFileArtifact( name + ".txt");
+    }
+
+    public void testSmokeTest()
+    {
+        TestSuiteResult tests = process();
+        assertEquals(5, tests.getFailures());
+        assertEquals(91, tests.getTotal());
+        assertEquals(0, tests.getErrors());
+    }
+
+    private TestSuiteResult process()
+    {
+        RegexTestPostProcessor pp = new RegexTestPostProcessor();
+        pp.setRegex("\\[(.*)\\].*<(.*)>");
+        pp.setStatusGroup(1);
+        pp.setNameGroup(2);
+        pp.setPassStatus("PASS");
+        pp.setFailureStatus("FAIL");
+
+        TestSuiteResult testResults = new TestSuiteResult();
+        CommandContext context = new CommandContext(null, tmpDir, testResults);
+        pp.process(artifact, result, context);
+        return testResults;
+    }    
+}
