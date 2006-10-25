436d344ea1361487787ef3f9d9fdab17d9744ef0
Jason Sankey <jason@zutubi.com>
2006-10-25 01:54:27 +0000

    Merged change 2361 from 1.1.x to trunk.

--- server-core/src/java/com/zutubi/pulse/bootstrap/SystemConfigurationSupport.java
+++ server-core/src/java/com/zutubi/pulse/bootstrap/SystemConfigurationSupport.java
@@ -1,5 +1,6 @@
 package com.zutubi.pulse.bootstrap;
 
+import com.opensymphony.util.TextUtils;
 import com.zutubi.pulse.config.CompositeConfig;
 import com.zutubi.pulse.config.Config;
 import com.zutubi.pulse.config.ConfigSupport;
@@ -19,7 +20,15 @@ public class SystemConfigurationSupport extends ConfigSupport implements SystemC
 
     public String getBindAddress()
     {
-        return getProperty(WEBAPP_BIND_ADDRESS, "0.0.0.0");
+        String result = config.getProperty(WEBAPP_BIND_ADDRESS);
+        if(TextUtils.stringSet(result))
+        {
+            return result;
+        }
+        else
+        {
+            return "0.0.0.0";
+        }
     }
 
     public int getServerPort()
