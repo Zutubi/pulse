fb667b86ed77bc86da1d234fda489d7524ba4339
Jason Sankey <jason@zutubi.com>
2006-12-03 13:18:40 +0000

    CIB-797: merged change 2682 from trunk back to 1.1.x.

--- server-core/src/java/com/zutubi/pulse/scm/ScmFilepathFilter.java
+++ server-core/src/java/com/zutubi/pulse/scm/ScmFilepathFilter.java
@@ -2,10 +2,11 @@ package com.zutubi.pulse.scm;
 
 import org.apache.tools.ant.types.selectors.SelectorUtils;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * <class-comment/>
  */
 public class ScmFilepathFilter implements FilepathFilter
 {
@@ -13,21 +14,47 @@ public class ScmFilepathFilter implements FilepathFilter
 
     public ScmFilepathFilter(List<String> excludedPaths)
     {
-        this.excludedPaths = excludedPaths;
+        this.excludedPaths = new ArrayList<String>(excludedPaths.size());
+        for(String pattern: excludedPaths)
+        {
+            this.excludedPaths.add(transformPath(pattern));
+        }
     }
 
     public boolean accept(String path)
     {
+        path = transformPath(path);
+
         if (excludedPaths != null)
         {
             for (String pattern : excludedPaths)
             {
-                if (SelectorUtils.matchPath(pattern, path))
+                // The Ant selector will only match a path starting with the
+                // file separator if the pattern also starts with the file
+                // separator.  This is sensible enough in Ant context, but
+                // here the SCMs can report paths like "/project/trunk/foo"
+                // or "//depot/bar".  A user will probably expect **/... to
+                // match in these cases, so we treat it as a special case.
+                String matchPath = path;
+                if(pattern.startsWith("**"))
+                {
+                    while(matchPath.startsWith(File.separator))
+                    {
+                        matchPath = matchPath.substring(1);
+                    }
+                }
+                
+                if (SelectorUtils.matchPath(pattern, matchPath))
                 {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    private String transformPath(String path)
+    {
+        return path.replace('/', File.separatorChar).replace('\\', File.separatorChar);
     }
 }
