d18d6a91c49a1fb47bc8ded1c78986f6c878e697
Daniel Ostermeier <daniel@zutubi.com>
2006-12-05 15:19:20 +0000

    CIB-794: Fix viewing html report artifacts from the build artifacts tab CIB-626: Added some support for predictable URLs. They are available for direct linking, but do not appear within the UI due to context path issues.

---
+++ master/src/java/com/zutubi/pulse/vfs/pulse/ArtifactFileObject.java
@@ -0,0 +1,134 @@
+package com.zutubi.pulse.vfs.pulse;
+
+import com.zutubi.pulse.core.model.CommandResult;
+import com.zutubi.pulse.core.model.StoredArtifact;
+import org.apache.commons.vfs.FileName;
+import org.apache.commons.vfs.FileType;
+import org.apache.commons.vfs.provider.AbstractFileSystem;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * The Artifact File Object represents a StoredArtifact instance.
+ * 
+ */
+public class ArtifactFileObject extends AbstractPulseFileObject implements ArtifactProvider, AddressableFileObject
+{
+    private final long artifactId;
+
+    private boolean isHtmlArtifact;
+
+    public ArtifactFileObject(final FileName name, final long artifactId, final AbstractFileSystem fs)
+    {
+        super(name, fs);
+
+        this.artifactId = artifactId;
+    }
+
+    public AbstractPulseFileObject createFile(final FileName fileName) throws Exception
+    {
+        File base = getArtifactBase();
+        File newBase = new File(base, fileName.getBaseName());
+
+        return objectFactory.buildBean(FileArtifactFileObject.class,
+                new Class[]{FileName.class, File.class, AbstractFileSystem.class},
+                new Object[]{fileName, newBase, pfs}
+        );
+    }
+
+    public File toFile()
+    {
+        return getArtifactBase();
+    }
+
+    protected void doAttach() throws Exception
+    {
+        StoredArtifact artifact = getArtifact();
+        isHtmlArtifact = artifact.hasIndexFile() && !getArtifact().isSingleFile();
+    }
+
+    private File getArtifactBase()
+    {
+        CommandResult result = getCommandResult();
+        StoredArtifact artifact = getArtifact();
+
+        File outputDir = result.getAbsoluteOutputDir(pfs.getConfigurationManager().getDataDirectory());
+        return new File(outputDir, artifact.getName());
+    }
+
+    protected FileType doGetType() throws Exception
+    {
+        if (isHtmlArtifact)
+        {
+            return FileType.FILE;
+        }
+        return FileType.FOLDER;
+    }
+
+    protected String[] doListChildren() throws Exception
+    {
+        if (isHtmlArtifact)
+        {
+            return new String[0];
+        }
+        return getArtifactBase().list();
+    }
+
+    protected long doGetContentSize() throws Exception
+    {
+        return 0;
+    }
+
+    protected InputStream doGetInputStream() throws Exception
+    {
+        return null;
+    }
+
+    public String getDisplayName()
+    {
+        return getArtifact().getName();
+    }
+
+    public List<String> getActions()
+    {
+        List<String> actions = new LinkedList<String>();
+        actions.add("archive");
+        if (isHtmlArtifact)
+        {
+            actions.add("html");
+        }
+        return actions;
+    }
+
+    public String getUrlPath()
+    {
+        if (isHtmlArtifact)
+        {
+            return "/file/artifacts/" + artifactId + "/" + getArtifact().findIndexFile();
+        }
+        return null;
+    }
+
+    public StoredArtifact getArtifact()
+    {
+        return buildManager.getArtifact(artifactId);
+    }
+
+    public long getArtifactId()
+    {
+        return artifactId;
+    }
+
+    public CommandResult getCommandResult()
+    {
+        return buildManager.getCommandResultByArtifact(artifactId);
+    }
+
+    public long getCommandResultId()
+    {
+        return getCommandResult().getId();
+    }
+}
