60874fcdbf543e68ef29eb303299bb4394a2b70a
Jason Sankey <jason@zutubi.com>
2007-02-07 06:56:06 +0000

    Switched over to YUI-EXT to layout the Pulse skin.  This allows us to nest complex layouts in the Pulse UI.  Several ugly bits of CSS have been cleaned up.  A skeletal project configuration page has been put in place.

---
+++ master/src/java/com/zutubi/pulse/web/admin/ProjectConfigurationAction.java
@@ -0,0 +1,13 @@
+package com.zutubi.pulse.web.admin;
+
+import com.zutubi.pulse.web.ActionSupport;
+
+/**
+ */
+public class ProjectConfigurationAction extends ActionSupport
+{
+    public String execute() throws Exception
+    {
+        return SUCCESS;
+    }
+}
