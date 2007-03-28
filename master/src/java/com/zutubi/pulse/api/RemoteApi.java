494f1b24f7175f2654788aa93359f9e633a19aab
Jason Sankey <jason@zutubi.com>
2007-03-28 07:08:40 +0000

    CIB-1023: ensure only admins can delete builds.

--- master/src/java/com/zutubi/pulse/api/RemoteApi.java
+++ master/src/java/com/zutubi/pulse/api/RemoteApi.java
@@ -261,16 +261,24 @@ public class RemoteApi implements com.zutubi.pulse.events.EventListener
 
     public boolean deleteBuild(String token, String projectName, int id) throws AuthenticationException
     {
-        tokenManager.verifyUser(token);
-        Project project = internalGetProject(projectName);
-        BuildResult build = buildManager.getByProjectAndNumber(project, id);
-        if (build == null)
+        tokenManager.loginUser(token);
+        try
         {
-            return false;
-        }
+            Project project = internalGetProject(projectName);
+            projectManager.checkWrite(project);
+            BuildResult build = buildManager.getByProjectAndNumber(project, id);
+            if (build == null)
+            {
+                return false;
+            }
 
-        buildManager.delete(build);
-        return true;
+            buildManager.delete(build);
+            return true;
+        }
+        finally
+        {
+            tokenManager.logoutUser();   
+        }
     }
 
     public Vector<Hashtable<String, Object>> getLatestBuildsForProject(String token, String projectName, String buildSpecification, boolean completedOnly, int maxResults) throws AuthenticationException
