c35f96350e1c2b1318790bd2b28bb10490daa641
Jason Sankey <jason@zutubi.com>
2007-07-16 13:31:46 +0000

    Added a read timeout to recipe result collection from slaves.

--- master/src/java/com/zutubi/pulse/SlaveBuildService.java
+++ master/src/java/com/zutubi/pulse/SlaveBuildService.java
@@ -97,7 +97,8 @@ public class SlaveBuildService implements BuildService
 
             URL resultUrl = new URL("http", slave.getHost(), slave.getPort(), "/download?token=" + serviceTokenManager.getToken() + "&project=" + UrlEncoded.encodeString(project) + "&spec=" + UrlEncoded.encodeString(spec) + "&incremental=" + incremental + "&output=" + output + "&recipe=" + recipeId);
             URLConnection urlConnection = resultUrl.openConnection();
-
+            urlConnection.setReadTimeout(120000);
+            
             // originally the zip stream was unzipped as read from the
             // servlet, however this resulted in socket errors on the
             // servlet side (I think when the zip was bigger than a
