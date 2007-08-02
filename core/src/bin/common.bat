901f71e0855fbc62c9bc974230116ed0c4bf0711
Jason Sankey <jason@zutubi.com>
2007-08-02 06:24:23 +0000

    CIB-1165: restore common.bat to its former non-glory but workaround it for dev packages.

--- core/src/bin/common.bat
+++ core/src/bin/common.bat
@@ -16,7 +16,7 @@ goto end
 
 set _JAVACMD=%JAVACMD%
 
-if not defined JAVA_HOME goto noJavaHome
+if not defined JAVA_HOME goto noJavaHomeS
 if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
 if not defined _JAVACMD set _JAVACMD=%JAVA_HOME%\bin\java.exe
 
