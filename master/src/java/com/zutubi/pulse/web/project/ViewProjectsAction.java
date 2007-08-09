f7e9b4db05f65dd28ee08956e0a5908922d8e966
Jason Sankey <jason@zutubi.com>
2007-08-09 06:59:26 +0000

    Merged change 3590 from 1.2.x to trunk.

--- master/src/java/com/zutubi/pulse/web/project/ViewProjectsAction.java
+++ master/src/java/com/zutubi/pulse/web/project/ViewProjectsAction.java
@@ -4,10 +4,12 @@ import com.zutubi.pulse.model.*;
 import com.zutubi.pulse.prototype.config.user.UserSettingsConfiguration;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * 
+ *
  *
  */
 public class ViewProjectsAction extends ProjectActionSupport
@@ -15,6 +17,8 @@ public class ViewProjectsAction extends ProjectActionSupport
     private List<ProjectGroup> groups;
     private List<Project> projects;
     private BuildColumns buildColumns;
+    private Map<Long, String> projectHealths = new HashMap<Long, String>();
+    private Map<Long, BuildResult> latestBuilds = new HashMap<Long, BuildResult>();
 
     public List<Project> getProjects()
     {
@@ -26,17 +30,14 @@ public class ViewProjectsAction extends ProjectActionSupport
         return groups;
     }
 
+    public String getHealth(Project project)
+    {
+        return projectHealths.get(project.getId());
+    }
+
     public BuildResult getLatestBuild(Project project)
     {
-        List<BuildResult> build = getBuildManager().getLatestBuildResultsForProject(project, 1);
-        if (build.size() == 0)
-        {
-            return null;
-        }
-        else
-        {
-            return build.get(0);
-        }
+        return latestBuilds.get(project.getId());
     }
 
     public BuildColumns getColumns()
@@ -50,16 +51,85 @@ public class ViewProjectsAction extends ProjectActionSupport
         Collections.sort(groups, new NamedEntityComparator());
 
         projects = getProjectManager().getAllProjectsCached();
-        for(ProjectGroup g: groups)
+        for (Project p : projects)
+        {
+            String health = "unknown";
+            BuildResult latest = null;
+
+            List<BuildResult> buildResults = getBuildManager().getLatestBuildResultsForProject(p, 2);
+            switch (buildResults.size())
+            {
+                case 0:
+                {
+                    // No latest and health not known
+                    break;
+                }
+                case 1:
+                {
+                    // A latest, health known if complete
+                    latest = buildResults.get(0);
+                    if (latest.completed())
+                    {
+                        health = getHealth(latest);
+                    }
+
+                    break;
+                }
+                case 2:
+                {
+                    latest = buildResults.get(0);
+                    if (latest.completed())
+                    {
+                        health = getHealth(latest);
+                    }
+                    else
+                    {
+                        health = getHealth(buildResults.get(1));
+                    }
+
+                    break;
+                }
+            }
+
+            if (latest != null)
+            {
+                latestBuilds.put(p.getId(), latest);
+            }
+
+            projectHealths.put(p.getId(), health);
+        }
+
+        for (ProjectGroup g : groups)
         {
             projects.removeAll(g.getProjects());
         }
-        
+
 //        Collections.sort(projects, new NamedEntityComparator());
 
         User user = getLoggedInUser();
         buildColumns = new BuildColumns(user == null ? UserSettingsConfiguration.defaultAllProjectsColumns() : user.getPreferences().getSettings().getAllProjectsColumns(), projectManager);
 
         return SUCCESS;
+    }
+
+    private String getHealth(BuildResult latest)
+    {
+        String health;
+        if (latest.succeeded())
+        {
+            if(latest.getWarningFeatureCount() > 0)
+            {
+                health = "warnings";
+            }
+            else
+            {
+                health = "ok";
+            }
+        }
+        else
+        {
+            health = "failed";
+        }
+        return health;
     }
 }
