d0b36f283fc3200c11ebb820d677a969e9a9b7d3
Jason Sankey <jason@zutubi.com>
2008-03-17 16:27:46 +0000

    Fixed validation for Perforce config.

--- bundles/com.zutubi.pulse.core.scm.p4/src/java/com/zutubi/pulse/core/scm/p4/config/PerforceConfiguration.java
+++ bundles/com.zutubi.pulse.core.scm.p4/src/java/com/zutubi/pulse/core/scm/p4/config/PerforceConfiguration.java
@@ -3,22 +3,24 @@ package com.zutubi.pulse.core.scm.p4.config;
 import com.zutubi.config.annotations.ConfigurationCheck;
 import com.zutubi.config.annotations.Form;
 import com.zutubi.config.annotations.SymbolicName;
-import com.zutubi.pulse.core.scm.ScmException;
 import com.zutubi.pulse.core.scm.config.ScmConfiguration;
 import com.zutubi.pulse.core.scm.p4.PerforceClient;
+import com.zutubi.validation.annotations.Required;
 
 /**
- *
- *
+ * Configures details of a Perforce depot and client.
  */
 @Form(fieldOrder = { "port", "user", "password", "spec", "monitor", "checkoutScheme", "customPollingInterval", "pollingInterval", "quietPeriodEnabled", "quietPeriod" })
 @ConfigurationCheck("PerforceConfigurationCheckHandler")
 @SymbolicName("zutubi.perforceConfig")
 public class PerforceConfiguration extends ScmConfiguration
 {
+    @Required
     private String port;
+    @Required
     private String user;
     private String password;
+    @Required
     private String spec;
 
     public String getPort()
