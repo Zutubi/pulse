c0103e59af257c1ce0d9dc841a650f7493e0d9a6
Jason Sankey <jason@zutubi.com>
2008-04-19 14:22:16 +0000

    Fixed acceptance test preparation script.

--- scripts/acceptance-test-prep.sh
+++ scripts/acceptance-test-prep.sh
@@ -36,52 +36,6 @@ unpack "$agentPackage" "$acceptDir"
 packageName="$(basename ${package%@(.tar.gz|.zip)})"
 agentPackageName="$(basename ${agentPackage%@(.tar.gz|.zip)})"
 
-# Now create an appropriate database.config.properties file
-case "$PULSE_DB" in
-    postgresql)
-        PULSE_DB_DRIVER=org.postgresql.Driver
-        PULSE_DB_URL=jdbc:postgresql://localhost:5432/pulse_accept
-        PULSE_DB_USER=pulsetest
-        PULSE_DB_PASSWORD=pulsetest
-        PULSE_DB_DIALECT=org.hibernate.dialect.PostgreSQLDialect
-
-        if echo 'SELECT * FROM pg_database;' | PGPASSWORD=pulsetest psql -h localhost -U pulsetest -d template1 |grep pulse_accept
-        then
-            echo 'DROP DATABASE pulse_accept;' | PGPASSWORD=pulsetest psql -h localhost -U pulsetest -d template1
-        fi
-        echo 'CREATE DATABASE pulse_accept;' | PGPASSWORD=pulsetest psql -h localhost -U pulsetest -d template1
-        ;;
-    mysql)
-        PULSE_DB_DRIVER=com.mysql.jdbc.Driver
-        PULSE_DB_URL=jdbc:mysql://localhost:3306/pulse_accept
-        PULSE_DB_USER=pulsetest
-        PULSE_DB_PASSWORD=pulsetest
-        PULSE_DB_DIALECT=org.hibernate.dialect.MySQLDialect
-
-        echo 'DROP SCHEMA IF EXISTS pulse_accept; CREATE SCHEMA pulse_accept;' | mysql --user=pulsetest --password=pulsetest
-        ;;
-    *)
-        PULSE_DB_DRIVER=org.hsqldb.jdbcDriver
-        PULSE_DB_URL=jdbc:hsqldb:DB_ROOT/db
-        PULSE_DB_USER=sa
-        PULSE_DB_PASSWORD=
-        PULSE_DB_DIALECT=org.hibernate.dialect.HSQLDialect
-        ;;
-esac
-
-cat > "$acceptDir/$packageName"/versions/*/system/config/database.properties.template << EOF
-jdbc.driverClassName=$PULSE_DB_DRIVER
-jdbc.url=$PULSE_DB_URL
-jdbc.username=$PULSE_DB_USER
-jdbc.password=$PULSE_DB_PASSWORD
-hibernate.dialect=$PULSE_DB_DIALECT
-hibernate.show_sql=false
-hibernate.jdbc.batch_size=0
-hibernate.jdbc.use_scrollable_resultsets=false
-EOF
-
-cp "$top/acceptance/src/test/drivers/"* "$acceptDir/$packageName"/versions/*/lib
-
 pushd "$acceptDir"
 export PULSE_HOME=
 
