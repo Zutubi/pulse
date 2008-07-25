5f5d0d97a0cecc15b7f114fd43322ed8547a0636
Jason Sankey <jason@zutubi.com>
2008-07-25 12:47:39 +0000

    CIB-1554: Cannot configure build completed trigger when a project has an apostrophe in the name.  Backported the JavascriptStringEncodeDirective and used it to escape things properly.

---
+++ master/src/java/com/zutubi/pulse/velocity/JavascriptStringEncodeDirective.java
@@ -0,0 +1,36 @@
+package com.zutubi.pulse.velocity;
+
+import freemarker.template.utility.StringUtil;
+import org.apache.velocity.context.InternalContextAdapter;
+import org.apache.velocity.exception.MethodInvocationException;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.apache.velocity.runtime.directive.Directive;
+import org.apache.velocity.runtime.parser.node.Node;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * A directive that encodes its content to escape Javascript string special
+ * characters (like ?js_string in FreeMarker).
+ */
+public class JavascriptStringEncodeDirective extends Directive
+{
+    public String getName()
+    {
+        return "jss";
+    }
+
+    public int getType()
+    {
+        return LINE;
+    }
+
+    public boolean render(InternalContextAdapter context, Writer writer, Node node) throws IOException, ResourceNotFoundException, ParseErrorException, MethodInvocationException
+    {
+        String in = String.valueOf(node.jjtGetChild(0).value(context));
+        writer.write(StringUtil.javaScriptStringEnc(in));
+        return true;
+    }
+}
