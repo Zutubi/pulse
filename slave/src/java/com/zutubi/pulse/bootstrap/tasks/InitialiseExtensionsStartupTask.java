edc8d1d0af3ca8f7268c50496018c38af6a18ceb
Jason Sankey <jason@zutubi.com>
2008-08-06 01:17:50 +0000

    Oops, the slave still needs extensions initialised.

---
+++ slave/src/java/com/zutubi/pulse/bootstrap/tasks/InitialiseExtensionsStartupTask.java
@@ -0,0 +1,23 @@
+package com.zutubi.pulse.bootstrap.tasks;
+
+import com.zutubi.pulse.bootstrap.ComponentContext;
+import com.zutubi.pulse.bootstrap.StartupTask;
+import com.zutubi.pulse.plugins.PluginManager;
+
+/**
+ * A startup task that instructs the plugin manager to initialise extension
+ * points.
+ */
+public class InitialiseExtensionsStartupTask implements StartupTask
+{
+    public void execute()
+    {
+        PluginManager pluginManager = (PluginManager) ComponentContext.getBean("pluginManager");
+        pluginManager.initialiseExtensions();
+    }
+
+    public boolean haltOnFailure()
+    {
+        return true;
+    }
+}
