34b8157b80000781df0b670b864877e7696f472a
Daniel Ostermeier <daniel@zutubi.com>
2008-08-14 04:23:58 +0000

    CIB-1603: We need to be running these tests as part of the acceptance suite.  If they fail then it looks like I will just have to bit the bullet and update the versions of the equinox patches correctly.

--- acceptance/src/test/com/zutubi/pulse/acceptance/AcceptanceTestSuite.java
+++ acceptance/src/test/com/zutubi/pulse/acceptance/AcceptanceTestSuite.java
@@ -46,61 +46,11 @@ public class AcceptanceTestSuite
 
         TestSuite main = new TestSuite();
         main.addTestSuite(JythonPackageFactoryTest.class); // check the support code works before running the acceptance test suite.
-//        main.addTestSuite(PluginUpgradeManagerAcceptanceTest.class);
+        main.addTestSuite(PluginUpgradeManagerAcceptanceTest.class);
         main.addTest(new AcceptanceTestSuiteSetupTeardown(suite));
 //        main.addTestSuite(AgentUpgradeAcceptanceTest.class);
         main.addTestSuite(StartupShutdownAcceptanceTest.class);
 
         return main;
-    }
-
-    public static Test otherSuite()
-    {
-        TestSuite suite = new TestSuite();
-        // need to run this one first as it runs through the iJnstallation setup.
-        suite.addTestSuite(SetupAcceptanceTest.class);
-
-        // now we can run the rest of the tests.
-
-        //---( xml rpc acceptance tests )---
-        suite.addTestSuite(ConfigXmlRpcAcceptanceTest.class);
-        suite.addTestSuite(ProjectXmlRpcAcceptanceTest.class);
-        suite.addTestSuite(ReportingXmlRpcAcceptanceTest.class);
-        suite.addTestSuite(ResourceXmlRpcAcceptanceTest.class);
-
-        //---( web ui acceptance tests )---
-        suite.addTestSuite(ConfigUIAcceptanceTest.class);
-        suite.addTestSuite(DeleteAcceptanceTest.class);
-        suite.addTestSuite(CloneAcceptanceTest.class);
-        suite.addTestSuite(ConfigActionsAcceptanceTest.class);
-        suite.addTestSuite(PluginUIAcceptanceTest.class);
-        suite.addTestSuite(GroupAcceptanceTest.class);
-        suite.addTestSuite(ProjectLabelAcceptanceTest.class);
-        suite.addTestSuite(AnonymousAccessAcceptanceTest.class);
-        suite.addTestSuite(LicenseAcceptanceTest.class);
-        suite.addTestSuite(DashboardAcceptanceTest.class);
-        suite.addTestSuite(ServerSectionAcceptanceTest.class);
-
-        //---( build tests )---
-        suite.addTestSuite(BuildAcceptanceTest.class);
-        suite.addTestSuite(BuildHookAcceptanceTest.class);
-        suite.addTestSuite(PersonalBuildAcceptanceTest.class);
-
-        //---( other acceptance tests )---
-//        suite.addTestSuite(StartupShutdownAcceptanceTest.class); - tries to launch pulse inline, does not work.
-
-//        return new AcceptanceTestSuiteSetupTeardown(suite);
-
-        TestSuite otherSuite = new TestSuite();
-        otherSuite.addTestSuite(StartupShutdownAcceptanceTest.class);
-        otherSuite.addTestSuite(PluginUpgradeManagerAcceptanceTest.class);
-        otherSuite.addTestSuite(AgentUpgradeAcceptanceTest.class);
-        otherSuite.addTestSuite(JythonPackageFactoryTest.class);
-
-        TestSuite globalSuite = new TestSuite();
-        globalSuite.addTest(new AcceptanceTestSuiteSetupTeardown(suite));
-        globalSuite.addTest(otherSuite);
-
-        return globalSuite;
     }
 }
