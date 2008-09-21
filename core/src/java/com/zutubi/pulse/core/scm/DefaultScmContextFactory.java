794e77d8df003e003ed9581cb0a7a5b6e4f7b2ef
Daniel Ostermeier <daniel@zutubi.com>
2008-09-21 07:26:41 +0000

    Changelist 4818 updates.

--- core/src/java/com/zutubi/pulse/core/scm/DefaultScmContextFactory.java
+++ core/src/java/com/zutubi/pulse/core/scm/DefaultScmContextFactory.java
@@ -5,21 +5,15 @@ import com.zutubi.pulse.core.scm.config.ScmConfiguration;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Default implementation of the scm context factory interface.
+ */
 public class DefaultScmContextFactory implements ScmContextFactory
 {
-    // would like access to the ConfigurationProvider here, but it is in master, and we are in core.
-    // would be nice to be able to 'lookup' the project configuration given the scm configuration, and
-    // determine the details from there.
-
     private File projectsDir;
 
     public ScmContext createContext(long id, ScmConfiguration config) throws ScmException
     {
-        // OPTIONS:
-        // a) DATA/projects/projectConfig.getProjectId()/scm/xxxxxxx
-        // b) DATA/work/<projectname>/.scm...
-        // going with a for now.
-        
         try
         {
             ScmContext context = new ScmContext();
@@ -36,7 +30,7 @@ public class DefaultScmContextFactory implements ScmContextFactory
         }
         catch (IOException e)
         {
-            throw new ScmException("IO Failure creating scm context. " + e.getMessage());
+            throw new ScmException("IO Failure creating scm context. " + e.getMessage(), e);
         }
     }
 
--- core/src/java/com/zutubi/pulse/core/scm/ScmContextFactory.java
+++ core/src/java/com/zutubi/pulse/core/scm/ScmContextFactory.java
@@ -2,6 +2,10 @@ package com.zutubi.pulse.core.scm;
 
 import com.zutubi.pulse.core.scm.config.ScmConfiguration;
 
+/**
+ * Interface for the scm context factory.  The context factory is used to maintain the
+ * scm context instances.  These instances hold persistent data between scm invocations.
+ */
 public interface ScmContextFactory
 {
     ScmContext createContext(long projectId, ScmConfiguration scm) throws ScmException;
