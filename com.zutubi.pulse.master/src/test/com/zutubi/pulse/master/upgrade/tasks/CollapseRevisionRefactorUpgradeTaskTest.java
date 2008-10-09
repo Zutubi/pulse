41e4253437efa4aa12041f255eb9b76c4f5756eb
Daniel Ostermeier <daniel@zutubi.com>
2008-10-09 02:28:23 +0000

    Added test cases.

--- com.zutubi.pulse.master/src/test/com/zutubi/pulse/master/upgrade/tasks/CollapseRevisionRefactorUpgradeTaskTest.java
+++ com.zutubi.pulse.master/src/test/com/zutubi/pulse/master/upgrade/tasks/CollapseRevisionRefactorUpgradeTaskTest.java
@@ -5,6 +5,8 @@ import com.zutubi.pulse.core.util.JDBCUtils;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Arrays;
 import java.util.List;
 
@@ -40,6 +42,113 @@ public class CollapseRevisionRefactorUpgradeTaskTest extends BaseUpgradeTaskTest
         assertColumnExists("BUILD_RESULT", "REVISION_ID");
         assertColumnNotExists("BUILD_RESULT", "REVISION_STRING");
 
+        runUpgrade();
+
+        assertTableNotExists("REVISION");
+        assertColumnExists("BUILD_CHANGELIST", "COMMENT");
+        assertColumnExists("BUILD_CHANGELIST", "AUTHOR");
+        assertColumnExists("BUILD_CHANGELIST", "TIME");
+        assertColumnNotExists("BUILD_CHANGELIST", "REVISION_AUTHOR");
+        assertColumnNotExists("BUILD_CHANGELIST", "REVISION_COMMENT");
+        assertColumnNotExists("BUILD_CHANGELIST", "REVISION_DATE");
+        assertColumnNotExists("BUILD_CHANGELIST", "REVISION_BRANCH");
+        assertColumnExists("BUILD_RESULT", "REVISION_STRING");
+        assertColumnNotExists("BUILD_RESULT", "REVISION_ID");
+    }
+
+    public void testDataTransferFromRevisionToBuildResult() throws SQLException, IOException
+    {
+        insertBuildRevision(1, 1, "rev1");
+        insertBuildRevision(2, 2, "rev2");
+        insertBuildRevision(3, 3, "rev3");
+
+        runUpgrade();
+
+        assertBuildResultRow(1, "rev1");
+        assertBuildResultRow(2, "rev2");
+        assertBuildResultRow(3, "rev3");
+    }
+
+    public void testUnexpectedNoRevisionForBuild() throws SQLException, IOException
+    {
+        insertBuildResult(1);
+        runUpgrade();
+        assertBuildResultRow(1, null);
+    }
+
+    private void assertBuildResultRow(long buildId, String revisionString) throws SQLException
+    {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try
+        {
+            con = dataSource.getConnection();
+            ps = con.prepareStatement("SELECT revision_string FROM build_result WHERE id = ?");
+            JDBCUtils.setLong(ps, 1, buildId);
+            rs = ps.executeQuery();
+            assertTrue(rs.next());
+            assertEquals(revisionString, JDBCUtils.getString(rs, "revision_string"));
+        }
+        finally
+        {
+            JDBCUtils.close(rs);
+            JDBCUtils.close(ps);
+            JDBCUtils.close(con);
+        }
+    }
+
+    private void insertBuildResult(long buildId) throws SQLException
+    {
+        Connection con = null;
+        PreparedStatement ps = null;
+        try
+        {
+            con = dataSource.getConnection();
+            ps = con.prepareStatement("INSERT INTO build_result (id, number) values (?, 1)");
+
+            JDBCUtils.setLong(ps, 1, buildId);
+            assertEquals(1, ps.executeUpdate());
+        }
+        finally
+        {
+            JDBCUtils.close(ps);
+            JDBCUtils.close(con);
+        }
+
+    }
+
+    private void insertBuildRevision(long buildId, long revisionId, String revisionString) throws SQLException
+    {
+        Connection con = null;
+        PreparedStatement ps = null;
+        try
+        {
+            con = dataSource.getConnection();
+            ps = con.prepareStatement("INSERT INTO revision (id, revisionstring, revisiontype) values (?, ?, 'type')");
+
+            JDBCUtils.setLong(ps, 1, revisionId);
+            JDBCUtils.setString(ps, 2, revisionString);
+            assertEquals(1, ps.executeUpdate());
+
+            JDBCUtils.close(ps);
+
+            ps = con.prepareStatement("INSERT INTO build_result (id, revision_id, number) values (?, ?, 1)");
+            JDBCUtils.setLong(ps, 1, buildId);
+            JDBCUtils.setLong(ps, 2, revisionId);
+            assertEquals(1, ps.executeUpdate());
+            
+        }
+        finally
+        {
+            JDBCUtils.close(ps);
+            JDBCUtils.close(con);
+        }
+
+    }
+
+    private void runUpgrade() throws SQLException, IOException
+    {
         Connection con = null;
         try
         {
@@ -51,17 +160,6 @@ public class CollapseRevisionRefactorUpgradeTaskTest extends BaseUpgradeTaskTest
         {
             JDBCUtils.close(con);
         }
-
-        assertTableNotExists("REVISION");
-        assertColumnExists("BUILD_CHANGELIST", "COMMENT");
-        assertColumnExists("BUILD_CHANGELIST", "AUTHOR");
-        assertColumnExists("BUILD_CHANGELIST", "TIME");
-        assertColumnExists("BUILD_RESULT", "REVISION_STRING");
-        assertColumnNotExists("BUILD_CHANGELIST", "REVISION_AUTHOR");
-        assertColumnNotExists("BUILD_CHANGELIST", "REVISION_COMMENT");
-        assertColumnNotExists("BUILD_CHANGELIST", "REVISION_DATE");
-        assertColumnNotExists("BUILD_CHANGELIST", "REVISION_BRANCH");
-        assertColumnNotExists("BUILD_RESULT", "REVISION_ID");
     }
 
     public void assertTableExists(String tableName)
