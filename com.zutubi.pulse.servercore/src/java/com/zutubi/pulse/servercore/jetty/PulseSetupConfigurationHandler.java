0fd5b19818d6a0c528d62eb61336e0d1d3aa81f1
Daniel Ostermeier <daniel@zutubi.com>
2009-01-21 06:01:04 +0000

    CIB-1467: place holder for the setup configuration work.

---
+++ com.zutubi.pulse.servercore/src/java/com/zutubi/pulse/servercore/jetty/PulseSetupConfigurationHandler.java
@@ -0,0 +1,13 @@
+package com.zutubi.pulse.servercore.jetty;
+
+import org.mortbay.jetty.Server;
+
+import java.io.IOException;
+
+public class PulseSetupConfigurationHandler implements ServerConfigurationHandler
+{
+    public void configure(Server server) throws IOException
+    {
+        // CIB-something.  Place holder for a later implementation.
+    }
+}
