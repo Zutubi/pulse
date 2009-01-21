0ee01e217c7991d2b758f2de8533fed305524e51
Jason Sankey <jason@zutubi.com>
2009-01-21 12:08:29 +0000

    CIB-1817: Options on the select database form should be disabled when embedded is selected.

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/tove/model/ControllingSelectFieldDescriptor.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/tove/model/ControllingSelectFieldDescriptor.java
@@ -27,7 +27,7 @@ public class ControllingSelectFieldDescriptor extends OptionFieldDescriptor
         addParameter(PARAM_DEPENDENT_FIELDS, deps);
     }
 
-    protected boolean displayMultiple()
+    protected boolean transformType()
     {
         return false;
     }
