a4143fe394a4fb797330a703d2661a54b3d4cbdd
Jason Sankey <jason@zutubi.com>
2009-01-30 21:44:31 +0000

    Merged change 5486 from 2.0.x to trunk.

---
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/upgrade/tasks/EnsureGlobalConfigPermanentUpgradeTask.java
@@ -0,0 +1,31 @@
+package com.zutubi.pulse.master.upgrade.tasks;
+
+import com.zutubi.tove.config.api.Configuration;
+import com.zutubi.tove.type.record.PathUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Adds the permanent flag to all global configuration if it is not already
+ * present.
+ */
+public class EnsureGlobalConfigPermanentUpgradeTask extends AbstractRecordPropertiesUpgradeTask
+{
+    private static final String SCOPE_SETTINGS = "settings";
+
+    public boolean haltOnFailure()
+    {
+        return false;
+    }
+
+    protected RecordLocator getRecordLocator()
+    {
+        return RecordLocators.newPathPattern(PathUtils.getPath(SCOPE_SETTINGS, PathUtils.WILDCARD_ANY_ELEMENT));
+    }
+
+    protected List<RecordUpgrader> getRecordUpgraders()
+    {
+        return Arrays.asList(RecordUpgraders.newAddMetaProperty(Configuration.PERMANENT_KEY, Boolean.toString(true)));
+    }
+}
