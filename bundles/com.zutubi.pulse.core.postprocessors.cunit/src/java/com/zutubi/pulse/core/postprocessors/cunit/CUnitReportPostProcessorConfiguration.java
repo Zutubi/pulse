fe2faa49bb9f2278fedd5c4acaaae58553d1a77e
Jason Sankey <jason@zutubi.com>
2009-02-02 18:48:57 +0000

    Converted CUnit support.

---
+++ bundles/com.zutubi.pulse.core.postprocessors.cunit/src/java/com/zutubi/pulse/core/postprocessors/cunit/CUnitReportPostProcessorConfiguration.java
@@ -0,0 +1,15 @@
+package com.zutubi.pulse.core.postprocessors.cunit;
+
+import com.zutubi.pulse.core.postprocessors.api.XMLTestReportPostProcessorConfigurationSupport;
+import com.zutubi.tove.annotations.SymbolicName;
+
+/**
+ */
+@SymbolicName("zutubi.cUnitReportPostProcessorConfig")
+public class CUnitReportPostProcessorConfiguration extends XMLTestReportPostProcessorConfigurationSupport
+{
+    public CUnitReportPostProcessorConfiguration()
+    {
+        super(CUnitReportPostProcessor.class, "CUnit");
+    }
+}
