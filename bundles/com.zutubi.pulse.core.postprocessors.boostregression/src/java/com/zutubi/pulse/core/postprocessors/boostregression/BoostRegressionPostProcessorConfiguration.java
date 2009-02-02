48aa613459a9606609d2bfab3255d51982e45a79
Jason Sankey <jason@zutubi.com>
2009-02-02 18:43:49 +0000

    Converted boost regression support.

---
+++ bundles/com.zutubi.pulse.core.postprocessors.boostregression/src/java/com/zutubi/pulse/core/postprocessors/boostregression/BoostRegressionPostProcessorConfiguration.java
@@ -0,0 +1,15 @@
+package com.zutubi.pulse.core.postprocessors.boostregression;
+
+import com.zutubi.pulse.core.postprocessors.api.XMLTestReportPostProcessorConfigurationSupport;
+import com.zutubi.tove.annotations.SymbolicName;
+
+/**
+ */
+@SymbolicName("zutubi.boostRegressionPostProcessorConfig")
+public class BoostRegressionPostProcessorConfiguration extends XMLTestReportPostProcessorConfigurationSupport
+{
+    public BoostRegressionPostProcessorConfiguration()
+    {
+        super(BoostRegressionPostProcessor.class, "Boost.Regression");
+    }
+}
