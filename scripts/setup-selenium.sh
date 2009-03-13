91b9931470ed889d7bd171e434bdb5b97e0d54f4
Jason Sankey <jason@zutubi.com>
2009-03-13 17:34:51 +0000

    Merged changes 5669 and 5673 from 2.0.x to trunk (upgrade to Selenium 1.0b2).

--- scripts/setup-selenium.sh
+++ scripts/setup-selenium.sh
@@ -6,7 +6,7 @@ set -e
 
 if ! netstat -an | grep \\b$port\\b > /dev/null
 then
-    java -jar "$top/com.zutubi.pulse.acceptance/src/test/misc/"selenium-server-*.jar -port $port > "$working/selenium-stdout.txt" 2> "$working/selenium-stderr.txt" &
+    java -jar "$top/com.zutubi.pulse.acceptance/src/test/misc/"selenium-server-*.jar -port $port -singleWindow > "$working/selenium-stdout.txt" 2> "$working/selenium-stderr.txt" &
     echo $! > "$pidfile"
 fi
 
