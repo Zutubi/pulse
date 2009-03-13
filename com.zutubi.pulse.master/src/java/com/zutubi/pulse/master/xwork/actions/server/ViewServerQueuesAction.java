fb47f8b2ec4ad420b7fb2a00c022fcb09749fbc8
Jason Sankey <jason@zutubi.com>
2009-03-13 11:53:44 +0000

    Response to review 221: instead of eliminating unviewable queue entries, show placeholder messages indicating there is something there (but you aren't allow to see it).

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/xwork/actions/server/ViewServerQueuesAction.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/xwork/actions/server/ViewServerQueuesAction.java
@@ -5,7 +5,7 @@ import com.zutubi.pulse.master.*;
 import com.zutubi.pulse.master.events.build.AbstractBuildRequestEvent;
 import com.zutubi.pulse.master.model.BuildManager;
 import com.zutubi.pulse.master.model.BuildResult;
-import com.zutubi.pulse.master.model.User;
+import com.zutubi.pulse.master.security.AcegiUtils;
 import com.zutubi.pulse.master.tove.config.project.ProjectConfigurationActions;
 import com.zutubi.pulse.master.xwork.actions.ActionSupport;
 import static com.zutubi.tove.security.AccessManager.ACTION_VIEW;
@@ -20,6 +20,7 @@ public class ViewServerQueuesAction extends ActionSupport
     private List<AbstractBuildRequestEvent> buildQueue;
     private List<BuildResult> executingBuilds;
     private List<RecipeAssignmentRequest> recipeQueueSnapshot;
+
     private FatController fatController;
     private RecipeQueue recipeQueue;
     private BuildManager buildManager;
@@ -51,46 +52,79 @@ public class ViewServerQueuesAction extends ActionSupport
 
     public String execute() throws Exception
     {
-        recipeQueueSnapshot = recipeQueue.takeSnapshot();
-        Iterator<RecipeAssignmentRequest> it = recipeQueueSnapshot.iterator();
-        while (it.hasNext())
-        {
-            RecipeAssignmentRequest assignmentRequest = it.next();
-            if (!accessManager.hasPermission(ACTION_VIEW, assignmentRequest.getProject()))
-            {
-                it.remove();
-            }
-        }
+        // We snapshot the queues with full privileges so we can show a more
+        // complete picture to the user by replacing entries they don't have
+        // the authority to view with placeholders.
+        snapshotQueuesAsSystem();
+        sortBuilds();
+        nullOutUnviewableEntries();
 
-        buildQueue = new LinkedList<AbstractBuildRequestEvent>();
-        executingBuilds = new LinkedList<BuildResult>();
+        return SUCCESS;
+    }
 
-        BuildQueue.Snapshot snapshot = fatController.snapshotBuildQueue();
-        for (Map.Entry<Entity,List<AbstractBuildRequestEvent>>  entityQueue: snapshot.getQueuedBuilds().entrySet())
+    private void snapshotQueuesAsSystem()
+    {
+        AcegiUtils.runAsSystem(new Runnable()
         {
-            Entity owner = entityQueue.getKey();
-            if (owner instanceof User || accessManager.hasPermission(ACTION_VIEW, owner))
+            public void run()
             {
-                buildQueue.addAll( entityQueue.getValue());
-            }
-        }
+                recipeQueueSnapshot = recipeQueue.takeSnapshot();
 
-        for (List<EntityBuildQueue.ActiveBuild> activeForEntity: snapshot.getActiveBuilds().values())
-        {
-            for (EntityBuildQueue.ActiveBuild activeBuild: activeForEntity)
-            {
-                BuildController buildController = activeBuild.getController();
-                if (accessManager.hasPermission(ACTION_VIEW, buildController.getProject()))
+                buildQueue = new LinkedList<AbstractBuildRequestEvent>();
+                executingBuilds = new LinkedList<BuildResult>();
+
+                BuildQueue.Snapshot snapshot = fatController.snapshotBuildQueue();
+                for (Map.Entry<Entity, List<AbstractBuildRequestEvent>>  entityQueue: snapshot.getQueuedBuilds().entrySet())
                 {
-                    BuildResult buildResult = buildManager.getBuildResult(buildController.getBuildId());
-                    if (buildResult != null && !buildResult.completed())
+                    buildQueue.addAll( entityQueue.getValue());
+                }
+
+                for (List<EntityBuildQueue.ActiveBuild> activeForEntity: snapshot.getActiveBuilds().values())
+                {
+                    for (EntityBuildQueue.ActiveBuild activeBuild: activeForEntity)
                     {
-                        executingBuilds.add(buildResult);
+                        BuildController buildController = activeBuild.getController();
+                        BuildResult buildResult = buildManager.getBuildResult(buildController.getBuildId());
+                        if (buildResult != null && !buildResult.completed())
+                        {
+                            executingBuilds.add(buildResult);
+                        }
                     }
                 }
             }
+        });
+    }
+
+    private void nullOutUnviewableEntries()
+    {
+        for (int i = 0; i < buildQueue.size(); i++)
+        {
+            if (!accessManager.hasPermission(ACTION_VIEW, buildQueue.get(i).getOwner()))
+            {
+                buildQueue.set(i, null);
+            }
         }
 
+        for (int i = 0; i < executingBuilds.size(); i++)
+        {
+            if (!accessManager.hasPermission(ACTION_VIEW, executingBuilds.get(i).getOwner()))
+            {
+                executingBuilds.set(i, null);
+            }
+        }
+
+        for (int i = 0; i < recipeQueueSnapshot.size(); i++)
+        {
+            if (!accessManager.hasPermission(ACTION_VIEW, recipeQueueSnapshot.get(i).getProject()))
+            {
+                recipeQueueSnapshot.set(i, null);
+            }
+
+        }
+    }
+
+    private void sortBuilds()
+    {
         Collections.sort(buildQueue, new Comparator<AbstractBuildRequestEvent>()
         {
             public int compare(AbstractBuildRequestEvent o1, AbstractBuildRequestEvent o2)
@@ -106,8 +140,6 @@ public class ViewServerQueuesAction extends ActionSupport
                 return (int) (o1.getStamps().getStartTime() - o2.getStamps().getStartTime());
             }
         });
-
-        return SUCCESS;
     }
 
     public void setFatController(FatController fatController)
