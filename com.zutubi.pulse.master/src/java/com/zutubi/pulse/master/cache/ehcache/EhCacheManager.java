1d8156f002048a359b75a01e8621ea20fb1272a9
Jason Sankey <jason@zutubi.com>
2009-03-15 17:11:55 +0000

    CIB-1890: Store caches in local temp directory to allow two Pulse instances on one box.

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/cache/ehcache/EhCacheManager.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/cache/ehcache/EhCacheManager.java
@@ -1,24 +1,52 @@
 package com.zutubi.pulse.master.cache.ehcache;
 
+import com.zutubi.pulse.servercore.bootstrap.SystemPaths;
+import com.zutubi.util.io.IOUtils;
 import net.sf.ehcache.CacheException;
 import net.sf.ehcache.CacheManager;
+import org.hsqldb.lib.StringInputStream;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
- * <class comment/>
+ * Handles creation and maintenance of database second-level caches.
  */
 public class EhCacheManager implements com.zutubi.pulse.master.cache.CacheManager
 {
-    private final CacheManager cacheManager;
+    private static final String CONFIG_FILE = "/ehcache-template.xml";
+    private static final String TEMP_DIR_TOKEN = "pulse.system.temp.dir";
 
-    public EhCacheManager()
+    private CacheManager cacheManager;
+    private SystemPaths systemPaths;
+
+    public void init() throws IOException
     {
+        String configContent;
+        InputStream stream = getClass().getResourceAsStream(CONFIG_FILE);
         try
         {
-            cacheManager = CacheManager.create();
+            configContent = IOUtils.inputStreamToString(stream);
+        }
+        finally
+        {
+            IOUtils.close(stream);
+            stream = null;
+        }
+
+        configContent = configContent.replace(TEMP_DIR_TOKEN, systemPaths.getTmpRoot().getAbsolutePath());
+        try
+        {
+            stream = new StringInputStream(configContent);
+            cacheManager = CacheManager.create(stream);
         }
         catch (CacheException e)
         {
             throw new RuntimeException(e);
+        }
+        finally
+        {
+            IOUtils.close(stream);
         }
     }
 
@@ -45,5 +73,10 @@ public class EhCacheManager implements com.zutubi.pulse.master.cache.CacheManage
             String cacheName = cacheManager.getCacheNames()[i];
             getCache(cacheName).removeAll();
         }
+    }
+
+    public void setSystemPaths(SystemPaths systemPaths)
+    {
+        this.systemPaths = systemPaths;
     }
 }
