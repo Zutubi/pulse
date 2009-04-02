db5d58ccdb942a0e0a7b7c34caf7e897cca86844
Daniel Ostermeier <daniel@zutubi.com>
2009-04-02 04:21:58 +0000

    Review 250 update.

--- com.zutubi.pulse.core/src/java/com/zutubi/pulse/core/dependency/ivy/IvyPatternValidator.java
+++ com.zutubi.pulse.core/src/java/com/zutubi/pulse/core/dependency/ivy/IvyPatternValidator.java
@@ -2,6 +2,8 @@ package com.zutubi.pulse.core.dependency.ivy;
 
 import com.zutubi.validation.ValidationException;
 import com.zutubi.validation.validators.StringFieldValidatorSupport;
+import com.zutubi.pulse.core.ReferenceResolver;
+import com.zutubi.pulse.core.ResolutionException;
 import org.apache.ivy.core.IvyPatternHelper;
 
 import java.util.Collections;
@@ -18,6 +20,19 @@ public class IvyPatternValidator extends StringFieldValidatorSupport
 
     protected void validateStringField(String value) throws ValidationException
     {
+        try
+        {
+            if (ReferenceResolver.containsReference(value))
+            {
+                // do not validate strings that contain references.
+                return;
+            }
+        }
+        catch (ResolutionException e)
+        {
+            // noop.
+        }
+
         try
         {
             IvyPatternHelper.substituteTokens(value, Collections.emptyMap());
