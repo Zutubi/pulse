4f4d0b617a932014e3cf7b48aa46023e5586b974
Daniel Ostermeier <daniel@zutubi.com>
2009-04-08 01:23:12 +0000

    Review 249 update. Do it properly.

--- com.zutubi.pulse.core/src/java/com/zutubi/pulse/core/dependency/ivy/IvyUtils.java
+++ com.zutubi.pulse.core/src/java/com/zutubi/pulse/core/dependency/ivy/IvyUtils.java
@@ -60,21 +60,6 @@ public class IvyUtils
      */
     public static String ivyEncodeStageName(String stageName)
     {
-        // Implementation note: this encoding is deliberately verbose to make it clearer what
-        // has been done when looking at the repository contents.  There is no practical reason
-        // to make this encoding obscure.
-
-        // Implementation note 2: this encoding gets around a number of issues.  Firstly, ivy config
-        // patterns do not support an escape character, so characters with special meaning need to be
-        // changed.  Secondly, the stage name is used as part of a URL.  If we were to use standard
-        // percentage encoding (as is used for special characters in URLs), then the repository will
-        // decode these and fail to write a file to the windows file system if it contains the '*'.
-        // The repository also interprets the ';' in the URL as an alias indicator, causing further
-        // problems.
-        // So, whilst the current encoding is not completely fool proof, it is pragmattic enough to
-        // work in the majority of cases and be clear enough to allow quick understanding of what is
-        // going on.
-
         StringBuffer buffer = new StringBuffer();
         for (int i = 0; i < stageName.length(); i++)
         {
@@ -82,22 +67,13 @@ public class IvyUtils
             switch (c)
             {
                 case ',':
-                    buffer.append("_comma_");
-                    break;
                 case ';':
-                    buffer.append("_semicolon_");
-                    break;
                 case '%':
-                    buffer.append("_percent_");
-                    break;
                 case '#':
-                    buffer.append("_hash_");
-                    break;
                 case '*':
-                    buffer.append("_star_");
-                    break;
                 case '=':
-                    buffer.append("_equals_");
+                case '_':
+                    buffer.append("_").append((int)c);
                     break;
                 default:
                     buffer.append(c);
