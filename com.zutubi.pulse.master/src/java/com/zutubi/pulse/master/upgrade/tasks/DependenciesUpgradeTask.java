9977d23eb9a3cf0089c84731fddb0871a6955f21
Daniel Ostermeier <daniel@zutubi.com>
2009-04-08 06:20:04 +0000

    CIB-1921: upgrade task for new dependencies configurations.  Includes the project/dependencies, project/triggers, project/stages/publications

---
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/upgrade/tasks/DependenciesUpgradeTask.java
@@ -0,0 +1,114 @@
+package com.zutubi.pulse.master.upgrade.tasks;
+
+import static com.zutubi.pulse.master.model.ProjectManager.GLOBAL_PROJECT_NAME;
+import com.zutubi.pulse.master.util.monitor.TaskException;
+import com.zutubi.tove.type.record.*;
+import com.zutubi.tove.transaction.TransactionManager;
+import com.zutubi.tove.transaction.UserTransaction;
+
+import java.util.Map;
+
+/**
+ * Introduces stubs for the new dependency configurations.
+ */
+public class DependenciesUpgradeTask extends AbstractUpgradeTask
+{
+    private static final String PATH_GLOBAL_DEPENDENCIES = "projects/" + GLOBAL_PROJECT_NAME + "/dependencies";
+    private static final String PATH_PATTERN_ALL_PROJECTS = "projects/*";
+
+    private static final String PROPERTY_NAME = "name";
+    private static final String PROPERTY_PROPERTIES = "properties";
+    private static final String PROPERTY_DEPENDENCIES = "dependencies";
+    private static final String PROPERTY_PUBLICATIONS = "publications";
+    private static final String PROPERTY_PUBLICATION_PATTERN = "publicationPattern";
+    private static final String PROPERTY_RETRIEVAL_PATTERN = "retrievalPattern";
+
+    private static final String DEPENDENCY_TRIGGER_NAME = "dependency trigger";
+
+    private static final String TYPE_DEPENDENCIES_CONFIGURATION = "";
+    private static final String TYPE_DEPENDENCY_TRIGGER = "";
+
+    private RecordManager recordManager;
+
+    private TransactionManager transactionManager;
+
+    public boolean haltOnFailure()
+    {
+        return true;
+    }
+
+    public void execute() throws TaskException
+    {
+        // Ensure that either all of the dependencies configuration changes are committed, or none.
+        UserTransaction txn = new UserTransaction(transactionManager);
+        txn.begin();
+
+        try
+        {
+            MutableRecord dependenciesSkeleton = new MutableRecordImpl();
+            dependenciesSkeleton.setSymbolicName(TYPE_DEPENDENCIES_CONFIGURATION);
+            dependenciesSkeleton.put(PROPERTY_DEPENDENCIES, new MutableRecordImpl());
+            dependenciesSkeleton.put(PROPERTY_PUBLICATIONS, new MutableRecordImpl());
+
+            MutableRecord stagePublicationsSkeleton = new MutableRecordImpl();
+
+            MutableRecord triggerSkeleton = new MutableRecordImpl();
+            triggerSkeleton.setSymbolicName(TYPE_DEPENDENCY_TRIGGER);
+            triggerSkeleton.put(PROPERTY_NAME, DEPENDENCY_TRIGGER_NAME);
+            triggerSkeleton.put(PROPERTY_PROPERTIES, new MutableRecordImpl());
+
+            // for all projects, add the skeletons.
+            RecordLocator allProjectsLocator = RecordLocators.newPathPattern(PATH_PATTERN_ALL_PROJECTS);
+            Map<String, Record> projects = allProjectsLocator.locate(recordManager);
+
+            for (Map.Entry<String, Record> entry : projects.entrySet())
+            {
+                String projectPath = entry.getKey();
+                MutableRecord projectEntry = entry.getValue().copy(true);
+                recordManager.insert(projectPath + "/dependencies", dependenciesSkeleton.copy(true));
+
+                MutableRecord stagesEntry = (MutableRecord) projectEntry.get("stages");
+                for (String stageName : stagesEntry.keySet())
+                {
+                    recordManager.insert(projectPath + "/stages/" + stageName + "/publications", stagePublicationsSkeleton.copy(true));
+                }
+
+                // Triggers are only associated with concrete projects.
+                if (!projectEntry.containsMetaKey(TemplateRecord.TEMPLATE_KEY))
+                {
+                    recordManager.insert(projectPath + "/triggers/" + triggerSkeleton.get(PROPERTY_NAME), triggerSkeleton.copy(true));
+                }
+            }
+
+            // for global project, add specific values.
+            Record globalDependencies = recordManager.select(PATH_GLOBAL_DEPENDENCIES);
+            MutableRecord globalDependenciesEntry = globalDependencies.copy(true);
+            globalDependenciesEntry.put(PROPERTY_PUBLICATION_PATTERN, "build/[artifact].[ext]");
+            globalDependenciesEntry.put(PROPERTY_RETRIEVAL_PATTERN, "lib/[artifact].[ext]");
+            recordManager.update(PATH_GLOBAL_DEPENDENCIES, globalDependenciesEntry);
+
+            txn.commit();
+            
+        }
+        catch (RuntimeException e)
+        {
+            txn.rollback();
+            throw e;
+        }
+        catch (Throwable t)
+        {
+            txn.rollback();
+            throw new RuntimeException(t);
+        }
+    }
+
+    public void setRecordManager(RecordManager recordManager)
+    {
+        this.recordManager = recordManager;
+    }
+
+    public void setPulseTransactionManager(TransactionManager transactionManager)
+    {
+        this.transactionManager = transactionManager;
+    }
+}
