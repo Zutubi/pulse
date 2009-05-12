3f3ea407a3a5a1314fb97528bb3fb57b2125da16
Jason Sankey <jason@zutubi.com>
2009-05-12 01:06:46 +0000

    CIB-1928: Simple agent priorities.

--- com.zutubi.pulse.master/src/test/com/zutubi/pulse/master/PrioritiserTest.java
+++ com.zutubi.pulse.master/src/test/com/zutubi/pulse/master/PrioritiserTest.java
@@ -4,12 +4,11 @@ import com.zutubi.pulse.core.test.api.PulseTestCase;
 import com.zutubi.pulse.master.agent.Agent;
 import com.zutubi.pulse.master.model.BuildResult;
 import com.zutubi.pulse.master.model.Project;
+import com.zutubi.pulse.master.tove.config.agent.AgentConfiguration;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.stub;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class PrioritiserTest extends PulseTestCase
 {
@@ -26,20 +25,31 @@ public class PrioritiserTest extends PulseTestCase
         prioritiser = new Prioritiser(request);
     }
 
+    public void testHigherPrioritySortsFirst()
+    {
+        Agent agent1 = mockAgent(-1);
+        Agent agent2 = mockAgent(10);
+        Agent agent3 = mockAgent(4);
+
+        List<Agent> agents = new LinkedList<Agent>(Arrays.asList(agent1, agent2, agent3));
+        Collections.sort(agents, prioritiser);
+        assertEquals(Arrays.asList(agent2, agent3, agent1), agents);
+    }
+
     public void testAgentNameSignficant()
     {
-        Agent agent1 = mock(Agent.class);
-        Agent agent2 = mock(Agent.class);
+        Agent agent1 = mockAgent(0);
+        Agent agent2 = mockAgent(0);
         stub(project.getName()).toReturn("project name");
         stub(agent1.getName()).toReturn("foo");
         stub(agent2.getName()).toReturn("bar");
 
         assertFalse(0 == prioritiser.compare(agent1, agent2));
     }
-    
+
     public void testCompareSameAgent()
     {
-        Agent agent = mock(Agent.class);
+        Agent agent = mockAgent(0);
         stub(agent.getName()).toReturn("foo");
 
         assertTrue(0 == prioritiser.compare(agent, agent));
@@ -55,7 +65,7 @@ public class PrioritiserTest extends PulseTestCase
         List<Agent> agents = new ArrayList<Agent>(10);
         for (int i = 0; i < 10; i++)
         {
-            Agent agent = mock(Agent.class);
+            Agent agent = mockAgent(0);
             stub(agent.getName()).toReturn("agent" + i);
             agents.add(agent);
         }
@@ -79,10 +89,10 @@ public class PrioritiserTest extends PulseTestCase
         Agent agents[] = new Agent[4];
         for (int i = 0; i < agents.length; i++)
         {
-            agents[i] = mock(Agent.class);
+            agents[i] = mockAgent(0);
             stub(agents[i].getName()).toReturn("agent" + i);
         }
-        
+
         for (Agent agent1 : agents)
         {
             // test irreflexive.
@@ -122,4 +132,12 @@ public class PrioritiserTest extends PulseTestCase
         }
     }
 
+    private Agent mockAgent(int priority)
+    {
+        Agent agent = mock(Agent.class);
+        AgentConfiguration config = new AgentConfiguration();
+        config.setPriority(priority);
+        stub(agent.getConfig()).toReturn(config);
+        return agent;
+    }
 }
