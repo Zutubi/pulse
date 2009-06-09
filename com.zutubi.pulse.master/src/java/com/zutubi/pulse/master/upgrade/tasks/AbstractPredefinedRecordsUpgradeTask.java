e4824cbf5723e1d51124b38640f337f383b02735
Jason Sankey <jason@zutubi.com>
2009-06-09 20:48:16 +0000

    CIB-1338: Allow the project reports to display custom data: Upgrade task.

---
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/upgrade/tasks/AbstractPredefinedRecordsUpgradeTask.java
@@ -0,0 +1,80 @@
+package com.zutubi.pulse.master.upgrade.tasks;
+
+import com.zutubi.pulse.core.util.ZipUtils;
+import com.zutubi.pulse.master.util.monitor.TaskException;
+import com.zutubi.pulse.servercore.bootstrap.SystemPaths;
+import com.zutubi.tove.type.record.DefaultRecordSerialiser;
+import com.zutubi.tove.type.record.MutableRecord;
+import com.zutubi.tove.type.record.RecordManager;
+import com.zutubi.util.FileSystemUtils;
+import com.zutubi.util.io.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Abstract base for upgrade tasks that load records from a pre-canned archive
+ * to insert them into the persistent config store.
+ */
+public abstract class AbstractPredefinedRecordsUpgradeTask extends AbstractUpgradeTask
+{
+    protected SystemPaths systemPaths;
+    protected RecordManager recordManager;
+
+    public void execute() throws TaskException
+    {
+        File tempDir = new File(systemPaths.getTmpRoot(), getClass().getName());
+        if (tempDir.exists() && !FileSystemUtils.rmdir(tempDir))
+        {
+            throw new TaskException("Unable to create clean up old temp directory '" + tempDir.getAbsolutePath() + "'");
+        }
+
+        if (!tempDir.mkdirs())
+        {
+            throw new TaskException("Unable to create temp directory '" + tempDir.getAbsolutePath() + "'");
+        }
+
+        try
+        {
+            execute(tempDir);
+        }
+        finally
+        {
+            FileSystemUtils.rmdir(tempDir);
+        }
+    }
+
+    protected abstract void execute(File tempDir) throws TaskException;
+
+    protected MutableRecord loadRecords(File tempDir, String name) throws TaskException
+    {
+        ZipInputStream stream = null;
+        try
+        {
+            Class<? extends AbstractPredefinedRecordsUpgradeTask> clazz = getClass();
+            stream = new ZipInputStream(clazz.getResourceAsStream(clazz.getSimpleName() + "." + name + ".zip"));
+            ZipUtils.extractZip(stream, tempDir);
+            DefaultRecordSerialiser serialiser = new DefaultRecordSerialiser(tempDir);
+            return serialiser.deserialise("");
+        }
+        catch (IOException e)
+        {
+            throw new TaskException(e);
+        }
+        finally
+        {
+            IOUtils.close(stream);
+        }
+    }
+
+    public void setSystemPaths(SystemPaths systemPaths)
+    {
+        this.systemPaths = systemPaths;
+    }
+
+    public void setRecordManager(RecordManager recordManager)
+    {
+        this.recordManager = recordManager;
+    }
+}
