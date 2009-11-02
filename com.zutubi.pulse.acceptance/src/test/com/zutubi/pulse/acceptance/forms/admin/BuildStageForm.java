de0ef5c985ae63ee0b8cf828e8dbfdc8eec66af8
Jason Sankey <jason@zutubi.com>
2009-11-02 10:15:39 +0000

    Response to review 426.

--- com.zutubi.pulse.acceptance/src/test/com/zutubi/pulse/acceptance/forms/admin/BuildStageForm.java
+++ com.zutubi.pulse.acceptance/src/test/com/zutubi/pulse/acceptance/forms/admin/BuildStageForm.java
@@ -1,8 +1,7 @@
 package com.zutubi.pulse.acceptance.forms.admin;
 
-import com.thoughtworks.selenium.Selenium;
-import com.zutubi.pulse.acceptance.forms.ConfigurationForm;
 import com.zutubi.pulse.acceptance.SeleniumBrowser;
+import com.zutubi.pulse.acceptance.forms.ConfigurationForm;
 import com.zutubi.pulse.master.tove.config.project.BuildStageConfiguration;
 
 /**
@@ -10,6 +9,8 @@ import com.zutubi.pulse.master.tove.config.project.BuildStageConfiguration;
  */
 public class BuildStageForm extends ConfigurationForm
 {
+    private static final int LAZY_LOAD_TIMEOUT = 30000;
+
     public BuildStageForm(SeleniumBrowser browser, boolean inherited)
     {
         super(browser, BuildStageConfiguration.class, true, inherited);
@@ -18,5 +19,18 @@ public class BuildStageForm extends ConfigurationForm
     public int[] getFieldTypes()
     {
         return new int[]{TEXTFIELD, TEXTFIELD, COMBOBOX};
+    }
+
+    @Override
+    public String[] getComboBoxOptions(String name)
+    {
+        // Lazily-loaded options require some effort: simulate a click and
+        // ensure we wait for the load to complete.
+        browser.evalExpression("var combo = selenium.browserbot.getCurrentWindow().Ext.getCmp('zfid." + name + "');" +
+                               "combo.store.on('load', function() { combo.loaded = true; });" +
+                               "combo.store.on('loadexception', function() { combo.loaded = true; });" +
+                               "combo.onTriggerClick();");
+        browser.waitForCondition("selenium.browserbot.getCurrentWindow().Ext.getCmp('zfid." + name + "').loaded", LAZY_LOAD_TIMEOUT);
+        return super.getComboBoxOptions(name);
     }
 }
