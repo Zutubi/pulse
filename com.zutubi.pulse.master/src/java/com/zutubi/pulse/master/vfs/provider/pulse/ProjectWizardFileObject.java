2d8df48317ac74e467581aafd1c35d8f3dbe0dc7
Jason Sankey <jason@zutubi.com>
2009-11-03 00:14:07 +0000

    Revert change as it breaks tests.

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/vfs/provider/pulse/ProjectWizardFileObject.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/vfs/provider/pulse/ProjectWizardFileObject.java
@@ -18,7 +18,6 @@ import org.apache.commons.vfs.FileName;
 import org.apache.commons.vfs.FileSystemException;
 import org.apache.commons.vfs.FileType;
 import org.apache.commons.vfs.provider.AbstractFileSystem;
-import org.apache.commons.vfs.provider.UriParser;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -96,9 +95,9 @@ public class ProjectWizardFileObject extends AbstractPulseFileObject implements 
         }
     }
 
-    private AbstractTypeWizard getWizardInstance() throws FileSystemException
+    private AbstractTypeWizard getWizardInstance()
     {
-        String path = PathUtils.getPath(MasterConfigurationRegistry.PROJECTS_SCOPE, UriParser.decode(getName().getBaseName()));
+        String path = PathUtils.getPath(MasterConfigurationRegistry.PROJECTS_SCOPE, getName().getBaseName());
         return (AbstractTypeWizard) ConfigurationWizardAction.getWizardInstance(path);
     }
 
