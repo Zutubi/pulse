06d520708fa5c1275f412bee28416b8573359d10
Jason Sankey <jason@zutubi.com>
2009-11-17 10:35:24 +0000

    CIB-2188: Ignore Perforce "Proxy could not update its cache" error.

--- bundles/com.zutubi.pulse.core.scm.p4/src/java/com/zutubi/pulse/core/scm/p4/PerforceErrorDetectingHandler.java
+++ bundles/com.zutubi.pulse.core.scm.p4/src/java/com/zutubi/pulse/core/scm/p4/PerforceErrorDetectingHandler.java
@@ -3,10 +3,18 @@ package com.zutubi.pulse.core.scm.p4;
 import com.zutubi.pulse.core.scm.api.ScmException;
 
 /**
+ * Perforce output handler that detects and reports errors.
  */
 public abstract class PerforceErrorDetectingHandler implements PerforceHandler
 {
+    /**
+     * Perforce may report this error during a sync but it does not affect the
+     * actual sync itself, so we can continue regardless.
+     */
+    private static final String ERROR_PROXY_CACHE = "Proxy could not update its cache";
+
     private boolean throwOnStderr = false;
+    private boolean haveSignificantError = false;
     private StringBuffer stderr;
 
     public PerforceErrorDetectingHandler(boolean throwOnStderr)
@@ -19,6 +27,11 @@ public abstract class PerforceErrorDetectingHandler implements PerforceHandler
     {
         stderr.append(line);
         stderr.append('\n');
+
+        if (!line.contains(ERROR_PROXY_CACHE))
+        {
+            haveSignificantError = true;
+        }
     }
 
     public void handleExitCode(int code) throws ScmException
@@ -35,7 +48,7 @@ public abstract class PerforceErrorDetectingHandler implements PerforceHandler
             throw new ScmException(message);
         }
 
-        if (stderr.length() > 0 && throwOnStderr)
+        if (haveSignificantError && throwOnStderr)
         {
             throw new ScmException("p4 process returned error '" + stderr.toString().trim() + "'");
         }
