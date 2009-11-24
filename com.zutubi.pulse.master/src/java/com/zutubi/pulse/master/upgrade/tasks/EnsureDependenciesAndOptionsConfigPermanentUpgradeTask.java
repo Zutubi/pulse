46c39ae80710d0009f67614e8522dd2cfdf56e16
Daniel Ostermeier <daniel@zutubi.com>
2009-11-24 01:04:37 +0000

    CIB-2197: include the project configuration options in the upgrade task, and fix a typo in the original :|

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/upgrade/tasks/EnsureDependenciesAndOptionsConfigPermanentUpgradeTask.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/upgrade/tasks/EnsureDependenciesAndOptionsConfigPermanentUpgradeTask.java
@@ -1,20 +1,22 @@
 package com.zutubi.pulse.master.upgrade.tasks;
 
 import com.zutubi.tove.config.api.Configuration;
-import com.zutubi.tove.type.record.PathUtils;
 import static com.zutubi.tove.type.record.PathUtils.WILDCARD_ANY_ELEMENT;
+import static com.zutubi.tove.type.record.PathUtils.getPath;
 
 import java.util.Arrays;
 import java.util.List;
 
 /**
- * Adds the permanent flag to all of the projects dependencies configurations if
- * it is not already present.
+ * Adds the permanent flag to all of the projects dependencies and options configurations
+ * if it is not already present.
  */
-public class EnsureDependenciesConfigPermanentUpgradeTask extends AbstractRecordPropertiesUpgradeTask
+public class EnsureDependenciesAndOptionsConfigPermanentUpgradeTask extends AbstractRecordPropertiesUpgradeTask
 {
     private static final String SCOPE_PROJECTS = "projects";
+    
     private static final String PROPERTY_DEPENDENCIES = "dependencies";
+    private static final String PROPERTY_OPTIONS = "options";
 
     public boolean haltOnFailure()
     {
@@ -23,7 +25,10 @@ public class EnsureDependenciesConfigPermanentUpgradeTask extends AbstractRecord
 
     protected RecordLocator getRecordLocator()
     {
-        return RecordLocators.newPath(PathUtils.getPath(SCOPE_PROJECTS, WILDCARD_ANY_ELEMENT, PROPERTY_DEPENDENCIES));
+        return RecordLocators.newPathPattern(
+                getPath(SCOPE_PROJECTS, WILDCARD_ANY_ELEMENT, PROPERTY_DEPENDENCIES),
+                getPath(SCOPE_PROJECTS, WILDCARD_ANY_ELEMENT, PROPERTY_OPTIONS)
+        );
     }
 
     protected List<RecordUpgrader> getRecordUpgraders()
