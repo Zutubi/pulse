0edb2e3cc6cea27e51931b99d4f14e7ffbb1e77c
Jason Sankey <jason@zutubi.com>
2009-12-10 20:00:51 +0000

    Take ant out of the equation.

--- com.zutubi.pulse.acceptance/src/test/com/zutubi/pulse/acceptance/dependencies/WaitAntProject.java
+++ com.zutubi.pulse.acceptance/src/test/com/zutubi/pulse/acceptance/dependencies/WaitAntProject.java
@@ -1,22 +1,27 @@
 package com.zutubi.pulse.acceptance.dependencies;
 
 import com.zutubi.pulse.acceptance.Constants;
-import com.zutubi.pulse.core.commands.ant.AntCommandConfiguration;
 import com.zutubi.pulse.core.commands.api.CommandConfiguration;
+import com.zutubi.pulse.core.commands.core.ExecutableCommandConfiguration;
 import com.zutubi.pulse.core.scm.config.api.CheckoutScheme;
 import com.zutubi.pulse.core.scm.config.api.ScmConfiguration;
 import com.zutubi.pulse.core.scm.svn.config.SubversionConfiguration;
 import com.zutubi.pulse.master.tove.config.project.ProjectConfiguration;
+import com.zutubi.pulse.master.tove.config.project.ProjectConfigurationWizard;
 import com.zutubi.util.FileSystemUtils;
 
 import java.io.File;
 import java.io.IOException;
+import static java.util.Arrays.asList;
 
 /**
  * A project configuration setup for working with the wait ant projects.
  */
 public class WaitAntProject extends ProjectConfigurationHelper
 {
+    private static final String JAR = "awaitfile.jar";
+    private static final String WAIT_TIMEOUT = "300";
+
     private File waitFile;
 
     public WaitAntProject(ProjectConfiguration config, File tmpDir)
@@ -32,8 +37,10 @@ public class WaitAntProject extends ProjectConfigurationHelper
     @Override
     public CommandConfiguration createDefaultCommand()
     {
-        AntCommandConfiguration command = (AntCommandConfiguration) super.createDefaultCommand();
-        command.setArgs(getFileArgument(waitFile));
+        ExecutableCommandConfiguration command = new ExecutableCommandConfiguration();
+        command.setName(ProjectConfigurationWizard.DEFAULT_COMMAND);
+        command.setExe("java");
+        command.setExtraArguments(asList("-jar", JAR, waitFile.getAbsolutePath().replace("\\", "/"), WAIT_TIMEOUT));
         return command;
     }
 
@@ -51,8 +58,4 @@ public class WaitAntProject extends ProjectConfigurationHelper
         FileSystemUtils.createFile(waitFile, "test");
     }
 
-    private String getFileArgument(File waitFile)
-    {
-        return "-Dfile=" + waitFile.getAbsolutePath().replace("\\", "/");
-    }
 }
