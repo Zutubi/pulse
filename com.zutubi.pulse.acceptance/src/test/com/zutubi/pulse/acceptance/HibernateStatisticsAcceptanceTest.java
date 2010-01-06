8e65f44b701ffc537535fa51bdb3153a1230d0f2
Jason Sankey <jason@zutubi.com>
2010-01-06 15:22:21 +0000

    Even more permissive waiting for hibernate stats to toggle.

--- com.zutubi.pulse.acceptance/src/test/com/zutubi/pulse/acceptance/HibernateStatisticsAcceptanceTest.java
+++ com.zutubi.pulse.acceptance/src/test/com/zutubi/pulse/acceptance/HibernateStatisticsAcceptanceTest.java
@@ -1,12 +1,15 @@
 package com.zutubi.pulse.acceptance;
 
 import com.zutubi.pulse.acceptance.pages.admin.HibernateStatisticsPage;
+import com.zutubi.util.Condition;
 
 /**
  * A sanity check to ensure that the hibernate statistics are displayable.
  */
 public class HibernateStatisticsAcceptanceTest extends SeleniumTestBase
 {
+    private static final int TIMEOUT = 60000;
+
     @Override
     protected void setUp() throws Exception
     {
@@ -24,19 +27,27 @@ public class HibernateStatisticsAcceptanceTest extends SeleniumTestBase
 
     public void testCanViewStatistics() throws Exception
     {
-        HibernateStatisticsPage statsPage = browser.openAndWaitFor(HibernateStatisticsPage.class);
+        final HibernateStatisticsPage statsPage = browser.openAndWaitFor(HibernateStatisticsPage.class);
         assertTrue(statsPage.isPresent());
         assertFalse(statsPage.isEnabled());
 
         statsPage.clickToggle();
-        browser.waitForPageToLoad();
-
-        assertTrue(statsPage.isEnabled());
+        AcceptanceTestUtils.waitForCondition(new Condition()
+        {
+            public boolean satisfied()
+            {
+                return statsPage.isEnabled();
+            }
+        }, TIMEOUT, "statistics to be enabled");
 
         statsPage.clickToggle();
-        browser.waitForPageToLoad();
-
-        assertFalse(statsPage.isEnabled());
+        AcceptanceTestUtils.waitForCondition(new Condition()
+        {
+            public boolean satisfied()
+            {
+                return !statsPage.isEnabled();
+            }
+        }, TIMEOUT, "statistics to be disabled");
     }
 
 }
