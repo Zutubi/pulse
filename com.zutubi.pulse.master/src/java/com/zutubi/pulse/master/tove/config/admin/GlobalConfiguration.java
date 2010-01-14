7205d779d0aa85f907eaa2c32f4a627f60cd46a0
Jason Sankey <jason@zutubi.com>
2010-01-14 10:40:47 +0000

    Doc link/string updates.

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/tove/config/admin/GlobalConfiguration.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/tove/config/admin/GlobalConfiguration.java
@@ -22,7 +22,7 @@ public class GlobalConfiguration extends AbstractConfiguration
 
     private String baseUrl;
     private String masterHost;
-    private String baseHelpUrl = "http://confluence.zutubi.com/display/pulse0200";
+    private String baseHelpUrl = "http://confluence.zutubi.com/display/pulse0201";
     private boolean rssEnabled = true;
     private boolean anonymousAccessEnabled = false;
     private boolean anonymousSignupEnabled = false;
