fdba2fe4a4f3395bc719756d69523389c28bee1d
Daniel Ostermeier <daniel@zutubi.com>
2010-01-19 06:32:22 +0000

    explicitly specify localhost.  Without this, svn will bind to an IPv6 address on windows 7, causing problems

--- scripts/setup-svn-repo.sh
+++ scripts/setup-svn-repo.sh
@@ -26,7 +26,7 @@ pulse1 = pulse1
 EOF
 
 gunzip -c "$dumpfile" | svnadmin load "$repodir"
-svnserve -d -r "$repodir" --listen-port=3088 --foreground &
+svnserve -d -r "$repodir" --listen-port=3088 --listen-host=127.0.0.1 --foreground &
 echo $! > "$pidfile"
 
 exit 0
