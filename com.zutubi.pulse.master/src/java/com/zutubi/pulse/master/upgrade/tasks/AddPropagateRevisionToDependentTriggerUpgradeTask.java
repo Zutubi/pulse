f57e8fd1245da2fb178dc6bb9366267507462798
Daniel Ostermeier <daniel@zutubi.com>
2010-02-22 03:53:43 +0000

    CIB-2299: upgrade tasks associated with this change.

---
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/upgrade/tasks/AddPropagateRevisionToDependentTriggerUpgradeTask.java
@@ -0,0 +1,32 @@
+package com.zutubi.pulse.master.upgrade.tasks;
+
+import com.zutubi.tove.type.record.PathUtils;
+
+import java.util.List;
+import java.util.Arrays;
+
+/**
+ * Add the propagate revision boolean property to dependency build triggers.
+ */
+public class AddPropagateRevisionToDependentTriggerUpgradeTask extends AbstractRecordPropertiesUpgradeTask
+{
+    private static final String TYPE = "zutubi.dependentBuildTriggerConfig";
+
+    protected RecordLocator getRecordLocator()
+    {
+        return RecordLocators.newTypeFilter(
+                RecordLocators.newPathPattern(PathUtils.getPath("projects/*/triggers/*")),
+                TYPE
+        );
+    }
+
+    protected List<RecordUpgrader> getRecordUpgraders()
+    {
+        return Arrays.asList(RecordUpgraders.newAddProperty("propagateRevision", "false"));
+    }
+
+    public boolean haltOnFailure()
+    {
+        return false;
+    }
+}
