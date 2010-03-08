0a94d8c48ebbc014461c79308b908821dc8e572c
Jason Sankey <jason@zutubi.com>
2010-03-08 04:11:15 +0000

    Response to review 577: documented TemplateNode interface.

--- com.zutubi.tove/src/java/com/zutubi/tove/config/TemplateNode.java
+++ com.zutubi.tove/src/java/com/zutubi/tove/config/TemplateNode.java
@@ -12,36 +12,115 @@ import java.util.List;
  */
 public interface TemplateNode
 {
+    /**
+     * Returns the parent of this node, if any.
+     * 
+     * @return the parent of this node, null if this is the hierarchy root
+     */
     TemplateNode getParent();
 
+    /**
+     * Returns a list of all children of this node.
+     * 
+     * @return an unmodifiable list of this node's children
+     */
     List<TemplateNode> getChildren();
 
+    /**
+     * Returns the direct child of this node with the given id, if such a child
+     * exists.
+     *  
+     * @param id the id of the child to find
+     * @return the child with the given id, or null if none is found
+     */
     TemplateNode getChild(String id);
 
+    /**
+     * Returns the path of the configuration represented by this node in the
+     * configuration system.  This path is independent of the template
+     * hierarchy.
+     *  
+     * @return the path of the configuration represented by this node
+     * 
+     * @see #getTemplatePath() 
+     */
     String getPath();
 
+    /**
+     * Returns a unique name for this node in the hierarchy.
+     * 
+     * @return a unique name for this node
+     */
     String getId();
 
+    /**
+     * Returns true if the configuration represented by this node is concrete,
+     * false if it is a template.
+     * 
+     * @return true if the configuration represented by this node is concrete
+     */
     boolean isConcrete();
 
+    /**
+     * Finds and returns the node with the given id, starting by trying this
+     * node followed by a depth-first search of all descendants.
+     *  
+     * @param id identifier of the node to find
+     * @return the non-strict descendant node with the given id, or null if no
+     *         such node is found
+     */
     TemplateNode findNodeById(String id);
 
+    /**
+     * Returns the path of this node from the hierarchy root.  The path is
+     * composed of node ids, separated by forward slashes.
+     * 
+     * @return the path of this node in the hierarchy
+     * 
+     * @see #getPath() 
+     */
     String getTemplatePath();
 
+    /**
+     * Returns the distance of this node from the root.  That is, the root has
+     * depth 0, its direct children depth 1, their direct children depth 2 and
+     * so on.
+     *  
+     * @return the depth of this node measured from the hierarchy root
+     */
     int getDepth();
 
+    /**
+     * Walks up the hierarchy from this node, executing the given callback on
+     * each node as it goes.  If the callback returns false, the walk is
+     * stopped at this node.
+     * 
+     * @param callback the callback to execute on each node
+     * @param strict   if true, this node is not included in the walk
+     */
     void forEachAncestor(NodeHandler callback, boolean strict);
 
+    /**
+     * Walks down the hierarchy from this node, executing the given callback on
+     * each node as it goes.  If the callback returns false, the walk is
+     * stopped at this node.
+     * 
+     * @param callback the callback to execute on each node
+     * @param strict   if true, this node is not included in the walk
+     */
     void forEachDescendant(NodeHandler callback, boolean strict);
 
+    /**
+     * Callback interface used by tree-walking methods.
+     */
     public interface NodeHandler
     {
         /**
          * Called with a node to be processed.  May optionally abort
-         * processing the descendants of this node.
+         * processing of further nodes.
          *
          * @param node the node to process
-         * @return true to process this node's descendants
+         * @return true to process further nodes
          */
         boolean handle(TemplateNode node);
     }
