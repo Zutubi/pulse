fea38be6f284e90ac8b603abcf7106811c387862
Daniel Ostermeier <daniel@zutubi.com>
2010-05-13 06:33:09 +0000

    Help warn of problems

--- scripts/setup-selenium.sh
+++ scripts/setup-selenium.sh
@@ -4,6 +4,12 @@ set -e
 
 . "$(dirname $0)/selenium-common.inc"
 
+if netstat -an | grep \\b$port\\b > /dev/null
+then
+    echo Can not start selenium, port $port is in use.
+    exit 1
+fi
+
 if ! netstat -an | grep \\b$port\\b > /dev/null
 then
     java -jar "$top/com.zutubi.pulse.acceptance/src/test/misc/"selenium-server-*.jar -port $port -singleWindow > "$working/selenium-stdout.txt" 2> "$working/selenium-stderr.txt" &
