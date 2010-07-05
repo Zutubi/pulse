600c2f2b7869e0475d75628d7bfe95cf35e78d15
Daniel Ostermeier <daniel@zutubi.com>
2010-07-05 05:06:18 +0000

    svn merge -r 8540:8541 svn+ssh://daniel@zutubi.com/svnroot/pulse/branches/2.1.x .

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/xwork/interceptor/ReadOnlyInterceptor.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/xwork/interceptor/ReadOnlyInterceptor.java
@@ -2,13 +2,10 @@ package com.zutubi.pulse.master.xwork.interceptor;
 
 import com.opensymphony.xwork.ActionInvocation;
 import com.opensymphony.xwork.interceptor.Interceptor;
-import com.zutubi.util.logging.Logger;
 import org.hibernate.FlushMode;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionException;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
 
 /**
  * An interceptor to wrap a read-only transaction around a request.  Used to
@@ -16,9 +13,6 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
  */
 public class ReadOnlyInterceptor implements Interceptor
 {
-    private static final Logger LOG = Logger.getLogger(ReadOnlyInterceptor.class);
-
-    private PlatformTransactionManager transactionManager;
     private SessionFactory sessionFactory;
 
     public void init()
@@ -31,55 +25,9 @@ public class ReadOnlyInterceptor implements Interceptor
 
     public String intercept(ActionInvocation invocation) throws Exception
     {
-        sessionFactory.getCurrentSession().setFlushMode(FlushMode.NEVER);
-
-        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
-        definition.setReadOnly(true);
-        TransactionStatus status = transactionManager.getTransaction(definition);
-        String result;
-        try
-        {
-            result = invocation.invoke();
-        }
-        catch (Exception e)
-        {
-            // Transactional code threw application exception -> rollback
-            rollbackOnException(status, e);
-            throw e;
-        }
-        catch (Error err)
-        {
-            // Transactional code threw error -> rollback
-            rollbackOnException(status, err);
-            throw err;
-        }
-
-        this.transactionManager.commit(status);
-        return result;
-
-    }
-
-    private void rollbackOnException(TransactionStatus status, Throwable ex) throws TransactionException
-    {
-        try
-        {
-            this.transactionManager.rollback(status);
-        }
-        catch (RuntimeException ex2)
-        {
-            LOG.error("Application exception overridden by rollback exception", ex);
-            throw ex2;
-        }
-        catch (Error err)
-        {
-            LOG.error("Application exception overridden by rollback error", ex);
-            throw err;
-        }
-    }
-
-    public void setTransactionManager(PlatformTransactionManager transactionManager)
-    {
-        this.transactionManager = transactionManager;
+        Session session = SessionFactoryUtils.getSession(sessionFactory, false);
+        session.setFlushMode(FlushMode.NEVER);
+        return invocation.invoke();
     }
 
     public void setSessionFactory(SessionFactory sessionFactory)
