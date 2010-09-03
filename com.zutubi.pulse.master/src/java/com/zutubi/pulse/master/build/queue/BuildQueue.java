5d35092bf0b2b32bff3f7d428b95212a725e0261
Daniel Ostermeier <daniel@zutubi.com>
2010-09-03 02:13:40 +0000

    CIB-2509: handle the case where an exception is generated during controller.start() where a null controller is getting associated with an activated request, causing future failures in viewing the server queue.

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/build/queue/BuildQueue.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/build/queue/BuildQueue.java
@@ -246,17 +246,40 @@ public class BuildQueue
             }
         }
 
+        List<ActivatedRequest> failedOnStart = new LinkedList<ActivatedRequest>();
+        
         // Now we go through and finish activating the newly activated requests.
         for (ActivatedRequest activatedRequest : toActivateRequests)
         {
-            // it may be worth taking this startup processing outside the synchronisation block since it may take some time.
             BuildController controller = buildControllerFactory.create(activatedRequest.getRequest());
-            long buildNumber = controller.start();
+            try
+            {
+                long buildNumber = controller.start();
 
-            activatedRequest.setController(controller);
+                activatedRequest.setController(controller);
 
-            buildRequestRegistry.requestActivated(activatedRequest.getRequest(), buildNumber);
-            eventManager.publish(new BuildActivatedEvent(this, activatedRequest.getRequest()), PublishFlag.DEFERRED);
+                buildRequestRegistry.requestActivated(activatedRequest.getRequest(), buildNumber);
+                eventManager.publish(new BuildActivatedEvent(this, activatedRequest.getRequest()), PublishFlag.DEFERRED);
+            }
+            catch (RuntimeException e)
+            {
+                failedOnStart.add(activatedRequest);
+                // if the build is persistent, then we mark it as activated as the build result will then contain
+                // all of the necessary details.  If not, then reject it with an error message.
+                if (controller.isBuildPersistent())
+                {
+                    buildRequestRegistry.requestActivated(activatedRequest.getRequest(), controller.getBuildNumber());
+                }
+                else
+                {
+                    buildRequestRegistry.requestRejected(activatedRequest.getRequest(), I18N.format("rejected.with.failure", e.getMessage()));
+                }
+            }
+        }
+
+        if (failedOnStart.size() > 0)
+        {
+            activatedRequests.removeAll(failedOnStart);
         }
     }
 
