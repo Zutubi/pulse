c4fca6ebc7a04829b3aa3892487772e3b9988f30
Daniel Ostermeier <daniel@zutubi.com>
2010-09-03 02:34:51 +0000

    CIB-2509: svn merge -r 8780:8781 svn+ssh://daniel@zutubi.com/svnroot/pulse/trunk .

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/build/control/BuildController.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/build/control/BuildController.java
@@ -1,5 +1,7 @@
 package com.zutubi.pulse.master.build.control;
 
+import com.zutubi.pulse.master.model.BuildResult;
+
 /**
  * The build controller interface is designed to provide an abstraction between
  * the build management and the build processing system.
@@ -10,10 +12,30 @@ package com.zutubi.pulse.master.build.control;
 public interface BuildController
 {
     /**
-     * Get the build result id associated with this build.
+     * Get the build result id associated with this build or -1 if no result
+     * is available.
+     * 
      * @return build result id.
+     *
+     * @see BuildController#isBuildPersistent()
      */
     long getBuildResultId();
+
+    /**
+     * Returns the build number of the associated build request, or -1 if no build
+     * number has been assigned.
+     *
+     * @return the results build number or -1. 
+     *
+     * @see BuildController#isBuildPersistent() 
+     */
+    long getBuildNumber();
+
+    /**
+     * @return true if the build controllers build result is persistent, false
+     * otherwise.
+     */
+    boolean isBuildPersistent();
 
     /**
      * Start the build.
