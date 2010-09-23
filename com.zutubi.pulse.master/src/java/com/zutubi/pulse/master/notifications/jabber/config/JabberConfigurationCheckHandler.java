7d847b191410a79601e9caafc3a8a9b815207eb1
Daniel Ostermeier <daniel@zutubi.com>
2010-09-23 07:32:21 +0000

    Review 84 update.

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/notifications/jabber/config/JabberConfigurationCheckHandler.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/notifications/jabber/config/JabberConfigurationCheckHandler.java
@@ -1,11 +1,10 @@
 package com.zutubi.pulse.master.notifications.jabber.config;
 
-import com.zutubi.pulse.master.notifications.jabber.JabberManager;
 import com.zutubi.pulse.core.api.PulseException;
+import com.zutubi.pulse.master.notifications.jabber.JabberManager;
 import com.zutubi.tove.annotations.SymbolicName;
 import com.zutubi.tove.annotations.Wire;
 import com.zutubi.tove.config.api.AbstractConfigurationCheckHandler;
-import org.jivesoftware.smack.XMPPException;
 
 @Wire
 @SymbolicName("zutubi.jabberConfigurationCheckHandler")
