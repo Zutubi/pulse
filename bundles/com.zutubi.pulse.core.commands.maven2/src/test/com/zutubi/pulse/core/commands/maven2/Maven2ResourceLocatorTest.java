412f257e8ba59754d44cc52c0be30bc9874d0e23
Jason Sankey <jason@zutubi.com>
2011-03-16 10:38:59 +0000

    Made maven 2 and 3 resource locator tests windows friendly.

--- bundles/com.zutubi.pulse.core.commands.maven2/src/test/com/zutubi/pulse/core/commands/maven2/Maven2ResourceLocatorTest.java
+++ bundles/com.zutubi.pulse.core.commands.maven2/src/test/com/zutubi/pulse/core/commands/maven2/Maven2ResourceLocatorTest.java
@@ -3,14 +3,16 @@ package com.zutubi.pulse.core.commands.maven2;
 import com.zutubi.pulse.core.resources.api.ResourceConfiguration;
 import com.zutubi.pulse.core.resources.api.ResourcePropertyConfiguration;
 import com.zutubi.pulse.core.resources.api.ResourceVersionConfiguration;
-import static com.zutubi.pulse.core.test.api.Matchers.matchesRegex;
 import com.zutubi.pulse.core.test.api.PulseTestCase;
-import static org.hamcrest.MatcherAssert.assertThat;
+import com.zutubi.util.FileSystemUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+
+import static com.zutubi.pulse.core.test.api.Matchers.matchesRegex;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Maven2ResourceLocatorTest extends PulseTestCase
 {
@@ -41,6 +43,6 @@ public class Maven2ResourceLocatorTest extends PulseTestCase
     {
         String path = System.getenv("MAVEN2_HOME");
         File file = new File(path);
-        return file.getCanonicalPath();
+        return FileSystemUtils.normaliseSeparators(file.getCanonicalPath());
     }
 }
