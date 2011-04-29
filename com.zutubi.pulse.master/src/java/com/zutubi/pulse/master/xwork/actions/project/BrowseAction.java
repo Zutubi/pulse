eb7bd6e50d68db46e2570d594310a98c454a39d1
Jason Sankey <jason@zutubi.com>
2011-04-29 03:01:21 +0000

    Removed the bang.

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/xwork/actions/project/BrowseAction.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/xwork/actions/project/BrowseAction.java
@@ -29,7 +29,7 @@ public class BrowseAction extends ActionSupport
         {
             BrowseViewConfiguration browseConfig = user == null ? new BrowseViewConfiguration() : user.getPreferences().getBrowseView();
             columnCount = browseConfig.getColumns().size();
-            throw new RuntimeException("Bang");
+            return SUCCESS;
         }
         else
         {
