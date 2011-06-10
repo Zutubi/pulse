cba8a6370d4af2061317f1a2859a75c59eefdaca
Jason Sankey <jason@zutubi.com>
2011-06-10 01:11:28 +0000

    Merged change 10762 from 2.3.x to trunk.

---
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/upgrade/tasks/RemoveChangeViewerDetailsUpgradeTask.java
@@ -0,0 +1,25 @@
+package com.zutubi.pulse.master.upgrade.tasks;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * CIB-2768: remove crufty details property from change viewers.
+ */
+public class RemoveChangeViewerDetailsUpgradeTask extends AbstractRecordPropertiesUpgradeTask
+{
+    protected RecordLocator getRecordLocator()
+    {
+        return RecordLocators.newPathPattern("projects/*/changeViewer");
+    }
+
+    protected List<RecordUpgrader> getRecordUpgraders()
+    {
+        return Arrays.asList(RecordUpgraders.newDeleteProperty("details"));
+    }
+
+    public boolean haltOnFailure()
+    {
+        return true;
+    }
+}
