56a6412e548487895c92850f6e7df9a5461b505e
Jason Sankey <jason@zutubi.com>
2013-03-09 09:13:49 +1100

    Switched away from ImmutableMap in MapSqueezerTest as it tests null keys and values.

--- com.zutubi.tove/src/test/com/zutubi/tove/squeezer/squeezers/MapSqueezerTest.java
+++ com.zutubi.tove/src/test/com/zutubi/tove/squeezer/squeezers/MapSqueezerTest.java
@@ -1,10 +1,10 @@
 package com.zutubi.tove.squeezer.squeezers;
 
-import com.google.common.collect.ImmutableMap;
 import com.zutubi.tove.squeezer.SqueezeException;
 import com.zutubi.util.junit.ZutubiTestCase;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -27,54 +27,54 @@ public class MapSqueezerTest extends ZutubiTestCase
 
     public void testSingleEntry() throws SqueezeException
     {
-        roundTrip(ImmutableMap.of("k1", "v1"));
+        roundTrip(mapOf("k1", "v1"));
     }
 
     public void testMultipleEntries() throws SqueezeException
     {
-        roundTrip(ImmutableMap.of("k1", "v1", "k2", "v2", "k3", "v3"));
+        roundTrip(mapOf("k1", "v1", "k2", "v2", "k3", "v3"));
     }
 
     public void testKeysAndValuesOfDifferentTypes() throws SqueezeException
     {
-        roundTrip(ImmutableMap.of("1", 1, "2", 2, "3", 3));
+        roundTrip(mapOf("1", 1, "2", 2, "3", 3));
     }
     
     public void testSeparatorCharacters() throws SqueezeException
     {
-        roundTrip(ImmutableMap.of("k:1", "v,1", "k,2", "v:2"));
+        roundTrip(mapOf("k:1", "v,1", "k,2", "v:2"));
     }
 
     public void testPercentCharacters() throws SqueezeException
     {
-        roundTrip(ImmutableMap.of("k%1", "v%1"));
+        roundTrip(mapOf("k%1", "v%1"));
     }
 
     public void testSomeNullKeys() throws SqueezeException
     {
-        roundTrip(ImmutableMap.of(null, "v1", 1, "v2"));
+        roundTrip(mapOf(null, "v1", 1, "v2"));
     }
 
     public void testAllNullKeys() throws SqueezeException
     {
-        roundTrip(ImmutableMap.of(null, "v1", null, "v2"));
+        roundTrip(mapOf(null, "v1", null, "v2"));
     }
 
     public void testSomeNullValues() throws SqueezeException
     {
-        roundTrip(ImmutableMap.of("k1", null, "k2", 2));
+        roundTrip(mapOf("k1", null, "k2", 2));
     }
 
     public void testAllNullValues() throws SqueezeException
     {
-        roundTrip(ImmutableMap.of("k1", null, "k2", null));
+        roundTrip(mapOf("k1", null, "k2", null));
     }
-    
+
     public void testUnsqueezableKey()
     {
         try
         {
-            squeezer.squeeze(ImmutableMap.of(new Unsqueezable(), 1));
+            squeezer.squeeze(mapOf(new Unsqueezable(), 1));
             fail("Should not be able to squeeze a map with an unsqueezable key");
         }
         catch (SqueezeException e)
@@ -87,7 +87,7 @@ public class MapSqueezerTest extends ZutubiTestCase
     {
         try
         {
-            squeezer.squeeze(ImmutableMap.of(1, new Unsqueezable()));
+            squeezer.squeeze(mapOf(1, new Unsqueezable()));
             fail("Should not be able to squeeze a map with an unsqueezable value");
         }
         catch (SqueezeException e)
@@ -100,7 +100,7 @@ public class MapSqueezerTest extends ZutubiTestCase
     {
         try
         {
-            squeezer.squeeze(ImmutableMap.of("1", 1, 2, 2));
+            squeezer.squeeze(mapOf("1", 1, 2, 2));
             fail("Should not be able to squeeze a map with keys of different types");
         }
         catch (SqueezeException e)
@@ -113,7 +113,7 @@ public class MapSqueezerTest extends ZutubiTestCase
     {
         try
         {
-            squeezer.squeeze(ImmutableMap.of(1, "1", 2, 2));
+            squeezer.squeeze(mapOf(1, "1", 2, 2));
             fail("Should not be able to squeeze a map with values of different types");
         }
         catch (SqueezeException e)
@@ -121,7 +121,25 @@ public class MapSqueezerTest extends ZutubiTestCase
             assertThat(e.getMessage(), containsString("Unable to squeeze maps with different classes"));
         }
     }
-    
+
+    /**
+     * Makes a map out of the given alternating keys and values.  We use null keys and values, so
+     * Guava collections may not be used.
+     *
+     * @param keysAndValues alternating keys and values
+     * @return a map containing the given keys and values
+     */
+    private Map<Object, Object> mapOf(Object... keysAndValues)
+    {
+        Map<Object, Object> result = new HashMap<Object, Object>();
+        for (int i = 0; i < keysAndValues.length / 2; i++)
+        {
+            result.put(keysAndValues[i * 2], keysAndValues[i * 2 + 1]);
+        }
+
+        return result;
+    }
+
     private void roundTrip(Map<?, ?> map) throws SqueezeException
     {
         assertEquals(map, squeezer.unsqueeze(squeezer.squeeze(map)));
