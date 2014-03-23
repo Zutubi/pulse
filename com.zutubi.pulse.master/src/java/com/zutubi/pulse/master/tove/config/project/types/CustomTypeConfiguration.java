8d7c4845836545ad197c6364635cf72d678850b1
Jason Sankey <jason@zutubi.com>
2014-03-24 10:21:48 +1100

    CIB-3100: Avoid validation of custom Pulse files except during editing.  Added a validation cache to the custom project type, so we only validate each unique Pulse XML file "once".

--- com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/tove/config/project/types/CustomTypeConfiguration.java
+++ com.zutubi.pulse.master/src/java/com/zutubi/pulse/master/tove/config/project/types/CustomTypeConfiguration.java
@@ -1,5 +1,10 @@
 package com.zutubi.pulse.master.tove.config.project.types;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 import com.zutubi.pulse.core.PulseScope;
 import com.zutubi.pulse.core.engine.FixedPulseFileProvider;
 import com.zutubi.pulse.core.engine.ProjectRecipesConfiguration;
@@ -14,10 +19,16 @@ import com.zutubi.tove.annotations.Transient;
 import com.zutubi.tove.annotations.Wire;
 import com.zutubi.util.StringUtils;
 import com.zutubi.util.bean.DefaultObjectFactory;
+import com.zutubi.util.logging.Logger;
 import com.zutubi.validation.Validateable;
 import com.zutubi.validation.ValidationContext;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 
 /**
  * A pulse file project where the pulse file is edited directly in Pulse
@@ -27,6 +38,12 @@ import java.io.ByteArrayInputStream;
 @Wire
 public class CustomTypeConfiguration extends TypeConfiguration implements Validateable
 {
+    private static final Logger LOG = Logger.getLogger(CustomTypeConfiguration.class);
+
+    private static final String VALIDATION_CACHE_SPEC = System.getProperty("pulse.custom.type.validation.cache.spec", "maximumSize=8192");
+    private static final Cache<HashCode, List<String>> VALIDATION_CACHE = CacheBuilder.from(VALIDATION_CACHE_SPEC).build();
+    private static final HashFunction VALIDATION_CACHE_HASH_FUNCTION = Hashing.goodFastHash(32);
+
     @TextArea(rows = 30, cols = 80)
     private String pulseFileString;
     @Transient
@@ -49,13 +66,13 @@ public class CustomTypeConfiguration extends TypeConfiguration implements Valida
 
     public void validate(ValidationContext context)
     {
-        if(!StringUtils.stringSet(pulseFileString))
+        if (!StringUtils.stringSet(pulseFileString))
         {
             context.addFieldError("pulseFileString", "pulse file is required");
             return;
         }
 
-        if(fileLoaderFactory == null)
+        if (fileLoaderFactory == null)
         {
             // This happens when we are validated during startup.  Although
             // it feels a bit sloppy, actually the only time it is really
@@ -64,28 +81,63 @@ public class CustomTypeConfiguration extends TypeConfiguration implements Valida
             return;
         }
 
+        // CIB-3100: Custom pulse file validation is expensive.  It's also likely (via templating,
+        // and instance cache refreshing) that we'll re-validate the same Pulse file XML many
+        // times.  So we cache the results of a validate, keyed by a hash of the Pulse file string
+        // (to keep memory requirements down).
+        final byte[] bytes = pulseFileString.getBytes();
         try
         {
-            PulseFileLoader loader = fileLoaderFactory.createLoader();
-            loader.setObjectFactory(new DefaultObjectFactory());
-
-            loader.load(new ByteArrayInputStream(pulseFileString.getBytes()), new ProjectRecipesConfiguration(), new PulseScope(), new ImportingNotSupportedFileResolver(), new CustomProjectValidationInterceptor());
-        }
-        catch(ParseException pe)
-        {
-            context.addActionError(pe.getMessage());
-            if(pe.getLine() > 0)
+            List<String> errors = VALIDATION_CACHE.get(VALIDATION_CACHE_HASH_FUNCTION.hashBytes(bytes), new Callable<List<String>>()
             {
-                String line = StringUtils.getLine(pulseFileString, pe.getLine());
-                if(line != null)
+                public List<String> call() throws Exception
                 {
-                    context.addActionError("First line of offending element: " + line);
+                    List<String> result = new ArrayList<String>();
+                    try
+                    {
+                        PulseFileLoader loader = fileLoaderFactory.createLoader();
+                        loader.setObjectFactory(new DefaultObjectFactory());
+
+                        loader.load(new ByteArrayInputStream(bytes), new ProjectRecipesConfiguration(), new PulseScope(), new ImportingNotSupportedFileResolver(), new CustomProjectValidationInterceptor());
+                    }
+                    catch (ParseException pe)
+                    {
+                        result.add(pe.getMessage());
+                        if (pe.getLine() > 0)
+                        {
+                            String line = StringUtils.getLine(pulseFileString, pe.getLine());
+                            if (line != null)
+                            {
+                                result.add("First line of offending element: " + line);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        result.add(e.getMessage());
+                    }
+
+                    if (result.size() == 0)
+                    {
+                        // The expected case, so share the single empty list to save memory.
+                        return Collections.emptyList();
+                    }
+                    else
+                    {
+                        return result;
+                    }
                 }
+            });
+
+            for (String error: errors)
+            {
+                context.addActionError(error);
             }
         }
-        catch(Exception e)
+        catch (ExecutionException e)
         {
-            context.addActionError(e.getMessage());
+            LOG.severe(e);
+            context.addActionError("Unable to validate: " + e.getMessage());
         }
     }
 
