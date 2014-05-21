6a0ae6598c543777810585755ad797ad457613dd
Jason Sankey <jason@zutubi.com>
2014-05-21 11:42:22 +1000

    Updated Windows service installation batch file.  The previous version was stale and broken.

--- com.zutubi.pulse.servercore/src/bin/install-service.bat
+++ com.zutubi.pulse.servercore/src/bin/install-service.bat
@@ -16,12 +16,14 @@ goto end
 if defined PROGRAMFILES(X86) (set ARCH=amd64) else (set ARCH=x86)
 
 "%PULSE_HOME%\bin\%ARCH%\PulseService" install ^
+  --Startup auto --Jvm auto --StartMode jvm --StopMode jvm ^
   --Classpath="%PULSE_HOME%\lib\boot.jar" ^
   --JvmMx=1024 ++JvmOptions=-XX:MaxPermSize=128m ^
   ++JvmOptions=-Dpulse.home="%PULSE_HOME%" ++JvmOptions=-Djava.awt.headless=true ^
   ++JvmOptions=-Djava.util.logging.config.class=com.zutubi.pulse.logging.ConsoleConfig ^
   --StartClass=com.zutubi.pulse.command.PulseCtl --StartParams=start ^
-  --StopClass=com.zutubi.pulse.command.PulseCtl --StopParams=stopservice
+  --StopClass=com.zutubi.pulse.command.PulseCtl --StopParams=stopservice ^
+  --LogPath="%PULSE_HOME%\logs" --StdOutput auto --StdError auto
 
 :end
 
