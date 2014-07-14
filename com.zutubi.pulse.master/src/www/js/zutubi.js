da60677bc456bfae178b1ea3a7f92c6f048347cf
Jason Sankey <jason@zutubi.com>
2014-07-14 11:52:50 +1000

    Updates for 2.7 release: fixed bundle versioning, updated version numbers in header and welcome page, set a build date.

--- com.zutubi.pulse.master/src/www/js/zutubi.js
+++ com.zutubi.pulse.master/src/www/js/zutubi.js
@@ -905,7 +905,7 @@ Zutubi.PulseHeader = Ext.extend(Ext.Toolbar, {
         }, this);
 
         this.addItem({xtype: 'tbtext', html: '&nbsp;::&nbsp;', tag: 'span'});
-        this.addItem({xtype: 'xztblink', text:"pulse 2.6", url: window.baseUrl + '/default.action'});
+        this.addItem({xtype: 'xztblink', text:"pulse 2.7", url: window.baseUrl + '/default.action'});
         this.addItem({xtype: 'tbtext', html: '&nbsp;::&nbsp;', tag: 'span'});
 
         if (this.projectName)
