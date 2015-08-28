c89da18e7919d552cf40a16e0a5ad05d512217b7
Jason Sankey <jason@zutubi.com>
2015-08-28 16:15:19 +1000

    CIB-3213: Allow build stage to be cancelled during dependency retrieval.  Hooked into Ivy's interruption, albeit in a slightly clunky way (as we don't properly use Ivy contexts).

--- com.zutubi.pulse.core/src/java/com/zutubi/pulse/core/dependency/ivy/RetrieveDependenciesCommand.java
+++ com.zutubi.pulse.core/src/java/com/zutubi/pulse/core/dependency/ivy/RetrieveDependenciesCommand.java
@@ -6,6 +6,7 @@ import com.zutubi.pulse.core.PulseExecutionContext;
 import com.zutubi.pulse.core.commands.api.Command;
 import com.zutubi.pulse.core.commands.api.CommandContext;
 import com.zutubi.pulse.core.engine.api.BuildException;
+import com.zutubi.pulse.core.engine.api.ExecutionContext;
 import com.zutubi.pulse.core.util.PulseZipUtils;
 import com.zutubi.tove.type.record.PathUtils;
 import com.zutubi.util.io.FileSystemUtils;
@@ -21,6 +22,8 @@ import javax.xml.transform.TransformerConfigurationException;
 import java.io.*;
 import java.net.URL;
 import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static com.zutubi.pulse.core.engine.api.BuildProperties.*;
 
@@ -37,6 +40,7 @@ public class RetrieveDependenciesCommand implements Command
     public static final String IVY_REPORT_FILE = "ivyreport.xml";
 
     private IvyClient ivy;
+    private volatile Thread retrieveThread;
 
     public RetrieveDependenciesCommand(RetrieveDependenciesCommandConfiguration config)
     {
@@ -56,66 +60,8 @@ public class RetrieveDependenciesCommand implements Command
             {
                 public Object call() throws Exception
                 {
-                    OutputStream outputStream = context.getOutputStream();
-                    if (outputStream == null)
-                    {
-                        outputStream = ByteStreams.nullOutputStream();
-                    }
-                    
-                    final PrintWriter outputWriter = new PrintWriter(outputStream);
-                    try
-                    {
-                        DefaultModuleDescriptor descriptor = context.getValue(NAMESPACE_INTERNAL, PROPERTY_DEPENDENCY_DESCRIPTOR, DefaultModuleDescriptor.class);
-                        String stageName = context.resolveVariables(context.getString(NAMESPACE_INTERNAL, PROPERTY_STAGE));
-                        String retrievalPattern = context.resolveVariables(context.getString(NAMESPACE_INTERNAL, PROPERTY_RETRIEVAL_PATTERN));
-                        boolean syncDestination = context.getBoolean(NAMESPACE_INTERNAL, PROPERTY_SYNC_DESTINATION, DEFAULT_SYNC_DESTINATION);
-                        boolean unzip = context.getBoolean(NAMESPACE_INTERNAL, PROPERTY_UNZIP_RETRIEVED_ARCHIVES, false);
-                        
-                        String targetPattern = PathUtils.getPath(context.getWorkingDir().getAbsolutePath(), retrievalPattern);
-                        IvyRetrievalReport retrievalReport = ivy.retrieveArtifacts(descriptor, stageName, targetPattern, syncDestination);
-
-                        captureRetrievalReport(retrievalReport, commandContext);
-
-                        if (retrievalReport.hasFailures())
-                        {
-                            // along with this recorded failure, ivy will report the details of the
-                            // problems encountered in the builds log.
-                            for (ArtifactDownloadReport failure : retrievalReport.getFailures())
-                            {
-                                commandContext.failure(failure.toString());
-                            }
-                        }
-                        else
-                        {
-                            outputWriter.println();
-                            outputWriter.println(I18N.format("retrieve.summary"));
-                            
-                            for (Artifact artifact : retrievalReport.getRetrievedArtifacts())
-                            {
-                                ModuleRevisionId mrid = artifact.getModuleRevisionId();
-                                String targetPath = IvyPatternHelper.substitute(targetPattern, artifact);
-                                outputWriter.println(I18N.format("retrieve.feedback", mrid.getName(), artifact.getName(), mrid.getRevision(), targetPath));
-                                File targetFile = new File(targetPath);
-                                if (unzip && targetPath.endsWith(".zip") && targetFile.isFile())
-                                {
-                                    File parentDir = targetFile.getParentFile();
-                                    outputWriter.println(I18N.format("unzipping", parentDir.getAbsolutePath()));
-                                    PulseZipUtils.extractZip(targetFile, parentDir);
-                                    outputWriter.println(I18N.format("unzipped"));
-                                    FileSystemUtils.delete(targetFile);
-                                }
-                            }
-                        }
-                        return null;
-                    }
-                    finally
-                    {
-                        // Don't close the writer: the underlying output stream
-                        // is used by subsequent commands.
-                        outputWriter.flush();
-
-                        ivy.cleanup(); // cleanup the cached artifacts, they are not needed after they have been delivered.
-                    }
+                    Future<Object> future = Executors.newSingleThreadExecutor().submit(new Retriever(commandContext));
+                    return future.get();
                 }
             });
         }
@@ -125,30 +71,114 @@ public class RetrieveDependenciesCommand implements Command
         }
     }
 
-    private void captureRetrievalReport(IvyRetrievalReport retrievalReport, CommandContext commandContext) throws IOException, TransformerConfigurationException, SAXException
+    public void terminate()
     {
-        final File outDir = commandContext.registerArtifact(OUTPUT_NAME, null, false, false, null);
-
-        File reportFile = new File(outDir, IVY_REPORT_FILE);
-        if (!outDir.isDirectory() && !outDir.mkdirs())
+        Thread thread = this.retrieveThread;
+        if (thread != null)
         {
-            throw new BuildException("Failed to create command output directory: " + outDir.getCanonicalPath());
-        }
-
-        FileOutputStream output = null;
-        try
-        {
-            output = new FileOutputStream(reportFile);
-            retrievalReport.toXml(output);
-        }
-        finally
-        {
-            IOUtils.close(output);
+            ivy.interrupt(thread);
         }
     }
 
-    public void terminate()
+    private class Retriever implements Callable<Object>
     {
+        private final CommandContext commandContext;
 
+        public Retriever(CommandContext commandContext)
+        {
+            this.commandContext = commandContext;
+        }
+
+        public Object call() throws Exception
+        {
+            ExecutionContext context = commandContext.getExecutionContext();
+            OutputStream outputStream = context.getOutputStream();
+            if (outputStream == null)
+            {
+                outputStream = ByteStreams.nullOutputStream();
+            }
+
+            ivy.pushContext();
+            retrieveThread = Thread.currentThread();
+            final PrintWriter outputWriter = new PrintWriter(outputStream);
+            try
+            {
+                DefaultModuleDescriptor descriptor = context.getValue(NAMESPACE_INTERNAL, PROPERTY_DEPENDENCY_DESCRIPTOR, DefaultModuleDescriptor.class);
+                String stageName = context.resolveVariables(context.getString(NAMESPACE_INTERNAL, PROPERTY_STAGE));
+                String retrievalPattern = context.resolveVariables(context.getString(NAMESPACE_INTERNAL, PROPERTY_RETRIEVAL_PATTERN));
+                boolean syncDestination = context.getBoolean(NAMESPACE_INTERNAL, PROPERTY_SYNC_DESTINATION, DEFAULT_SYNC_DESTINATION);
+                boolean unzip = context.getBoolean(NAMESPACE_INTERNAL, PROPERTY_UNZIP_RETRIEVED_ARCHIVES, false);
+
+                String targetPattern = PathUtils.getPath(context.getWorkingDir().getAbsolutePath(), retrievalPattern);
+                IvyRetrievalReport retrievalReport = ivy.retrieveArtifacts(descriptor, stageName, targetPattern, syncDestination);
+
+                captureRetrievalReport(retrievalReport, commandContext);
+
+                if (retrievalReport.hasFailures())
+                {
+                    // along with this recorded failure, ivy will report the details of the
+                    // problems encountered in the builds log.
+                    for (ArtifactDownloadReport failure : retrievalReport.getFailures())
+                    {
+                        commandContext.failure(failure.toString());
+                    }
+                }
+                else
+                {
+                    outputWriter.println();
+                    outputWriter.println(I18N.format("retrieve.summary"));
+
+                    for (Artifact artifact : retrievalReport.getRetrievedArtifacts())
+                    {
+                        ModuleRevisionId mrid = artifact.getModuleRevisionId();
+                        String targetPath = IvyPatternHelper.substitute(targetPattern, artifact);
+                        outputWriter.println(I18N.format("retrieve.feedback", mrid.getName(), artifact.getName(), mrid.getRevision(), targetPath));
+                        File targetFile = new File(targetPath);
+                        if (unzip && targetPath.endsWith(".zip") && targetFile.isFile())
+                        {
+                            File parentDir = targetFile.getParentFile();
+                            outputWriter.println(I18N.format("unzipping", parentDir.getAbsolutePath()));
+                            PulseZipUtils.extractZip(targetFile, parentDir);
+                            outputWriter.println(I18N.format("unzipped"));
+                            FileSystemUtils.delete(targetFile);
+                        }
+                    }
+                }
+                return null;
+            }
+            finally
+            {
+                // Don't close the writer: the underlying output stream
+                // is used by subsequent commands.
+                outputWriter.flush();
+
+                ivy.cleanup(); // cleanup the cached artifacts, they are not needed after they have been delivered.
+
+                retrieveThread = null;
+                ivy.popContext();
+            }
+        }
+
+        private void captureRetrievalReport(IvyRetrievalReport retrievalReport, CommandContext commandContext) throws IOException, TransformerConfigurationException, SAXException
+        {
+            final File outDir = commandContext.registerArtifact(OUTPUT_NAME, null, false, false, null);
+
+            File reportFile = new File(outDir, IVY_REPORT_FILE);
+            if (!outDir.isDirectory() && !outDir.mkdirs())
+            {
+                throw new BuildException("Failed to create command output directory: " + outDir.getCanonicalPath());
+            }
+
+            FileOutputStream output = null;
+            try
+            {
+                output = new FileOutputStream(reportFile);
+                retrievalReport.toXml(output);
+            }
+            finally
+            {
+                IOUtils.close(output);
+            }
+        }
     }
 }
